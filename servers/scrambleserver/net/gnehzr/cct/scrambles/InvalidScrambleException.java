f287b75ae51929f020e2a18264522a3126d66cb0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2010-05-20 05:50:33 -0700

    Deleted!

--- servers/scrambleserver/net/gnehzr/cct/scrambles/InvalidScrambleException.java
+++
@@ -1,8 +0,0 @@
-package net.gnehzr.cct.scrambles;
-
-@SuppressWarnings("serial")
-public class InvalidScrambleException extends Exception {
-	public InvalidScrambleException(String scramble) {
-		super("Invalid scramble: " + scramble);
-	}
-}
