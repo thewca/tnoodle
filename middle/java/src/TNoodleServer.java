014c5335d3fff8d53f42c5d93e7a61e17faddab2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2010-05-21 00:37:48 -0700

    Changed file permissions of everything.

--- middle/java/src/TNoodleServer.java
+++ middle/java/src/TNoodleServer.java
@@ -1,12 +1,37 @@
 import java.awt.Image;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public abstract class TNoodleServer {
-	public TNoodleServer(String url, int port) {
-		
+	private URL scrambler, viewer;
+	public TNoodleServer(String urlStr) {
+		String result = null;
+		if(!urlStr.startsWith("http://")) {
+			// TODO
+		}
+		scrambler = new URL(urlStr + "/scrambler");
+		scrambler = new URL(urlStr + "/viewer");
+
+		try {
+			URLConnection conn = url.openConnection();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			StringBuffer sb = new StringBuffer();
+			String line;
+			while((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			rd.close();
+			result = sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public abstract String[] getAvailablePuzzles();
+
 	public abstract String[] getScramble(String puzzle);
+
 	public abstract Image getScrambleImage(String puzzle, String scramble);
 }
