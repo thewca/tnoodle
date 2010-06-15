8b914f3036775cfd70f248390efdabd6ecc14e06
Jeremy Fleischman <jeremyfleischman@gmail.com>
2010-06-14 18:18:47 -0700

    Opps, here's the rest.

--- servers/scrambleserver/src/net/gnehzr/tnoodle/servers/scrambleserver/InvalidScrambleException.java
+++
@@ -1,8 +0,0 @@
-package net.gnehzr.tnoodle.servers.scrambleserver;
-
-@SuppressWarnings("serial")
-public class InvalidScrambleException extends Exception {
-	public InvalidScrambleException(String scramble) {
-		super("Invalid scramble: " + scramble);
-	}
-}
