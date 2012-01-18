d3a99546cc7b6d35b9d0ee918713b5ea19b946ef
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-01-18 13:11:54 -0800

    Hopefully fixed submodule mess.

---
+++ twisty.js/tmtproject.py
@@ -0,0 +1,13 @@
+import tmt
+
+class Project(tmt.TmtProject):
+	def getDependencies(self):
+		return []
+
+	def compile(self):
+		pass
+
+	def clean(self):
+		pass
+
+Project(tmt.projectName(), description="Lucas Garron's Javascript puzzle simulator.")
