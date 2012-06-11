12752059ae7d6f3e46dc21b50b6eb1764b458bd3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-06-11 05:10:39 -0700

    Added profiling of initalization time.

--- Search.java
+++ Search.java
@@ -22,6 +22,8 @@ public class Search {
 
 	
 	public static void main(String[] args) {
+		long t = System.nanoTime();
+
 		FullCube f;// = new FullCube("");
 //		System.out.println(f.getParity());
 //		System.out.println(f.getShapeIdx());
@@ -29,7 +31,10 @@ public class Search {
 		int a = Square.SquarePrun[0];
 		Random gen = new Random(1000L);
 		new Search().solution(new FullCube(""));
-		long t = System.nanoTime();
+		System.out.println((System.nanoTime()-t)/1e9 + " seconds to initialize");
+
+
+		t = System.nanoTime();
 		for (int x=0; x<1000; x++) {
 		
 	//		System.out.println(m);
@@ -40,7 +45,7 @@ public class Search {
 			Search s = new Search();
 //			s.solution(FullCube.randomCube());
 			System.out.println(s.solution(FullCube.randomCube()));
-			System.out.println((System.nanoTime()-t)/1000000.0/x);
+			System.out.println((System.nanoTime()-t)/1e9/(x+1));
 //			t = System.nanoTime();
 		}
 
