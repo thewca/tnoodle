146d8b3975b3c22afe32582df6dd71e33cf86c04
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-06-13 23:58:06 -0700

    Added optimization to avoid recompiling the 4x4 tables even when fivephase needs to be recompiled. This will be a problem if the format of the tables ever changes, but hopefully when/if someone does that, they are aware enough to know to delete their fivephase/CACHED_FIVEPHASE_TABLES_DELETE_TO_RECREATE file.

--- fivephase/tmtproject.py
+++ fivephase/tmtproject.py
@@ -6,17 +6,25 @@ class Project(tmt.EclipseProject):
 	def configure(self):
 		tmt.EclipseProject.configure(self)
 		self.fivephase_tables = 'fivephase_tables'
+		self.CACHED_FIVEPHASE_TABLES = join(self.name, 'CACHED_FIVEPHASE_TABLES_DELETE_TO_RECREATE')
 
 	def innerCompile(self, src, tempBin, bin):
-		# build the pruning and transition tables for the five phase algorithm
-		tablesPath = join(tempBin, self.fivephase_tables)
-		print "Generating %s" % tablesPath
-		cp = self.getClasspathEntities()
-		cp.add(tempBin) # getClasspathEntities() contains self.bin, which isn't up to date yet
-		assert 0 == tmt.java(
-						main="cg.fivestage444.Tools",
-						classpath=self.toClasspath(cp),
-						args=[ tablesPath ])
-		print "Successfully generated %s!" % tablesPath
+		if src == self.src:
+			tablesPath = join(tempBin, self.fivephase_tables)
+			if not exists(self.CACHED_FIVEPHASE_TABLES):
+				# build the pruning and transition tables for the five phase algorithm
+				print "Generating %s" % self.CACHED_FIVEPHASE_TABLES
+				cp = self.getClasspathEntities()
+				cp.add(tempBin) # getClasspathEntities() contains self.bin, which isn't up to date yet
+				assert 0 == tmt.java(
+									main="cg.fivestage444.Tools",
+									classpath=self.toClasspath(cp),
+									args=[ self.CACHED_FIVEPHASE_TABLES ])
+				print "Successfully generated %s!" % self.CACHED_FIVEPHASE_TABLES
+			else:
+				print "******************************************************"
+				print "I found %s, so I'm just going to copy it into %s because I'm sure you're in a rush. If this isn't what you want, please delete %s. Cleaning will NOT remove this file." % ( self.CACHED_FIVEPHASE_TABLES, tablesPath, self.CACHED_FIVEPHASE_TABLES )
+				print "******************************************************"
+			shutil.copy(self.CACHED_FIVEPHASE_TABLES, tablesPath)
 
 Project(tmt.projectName(), description="A copy of Clement Gallet's (https://github.com/clementgallet) 4x4 scrambler.")
--- twophase/tmtproject.py
+++ twophase/tmtproject.py
@@ -8,15 +8,16 @@ class Project(tmt.EclipseProject):
 		self.twophase_tables = 'twophase_tables'
 
 	def innerCompile(self, src, tempBin, bin):
-		# build the pruning and transition tables for the two phase algorithm
-		tablesPath = join(tempBin, self.twophase_tables)
-		print "Generating %s" % tablesPath
-		cp = self.getClasspathEntities()
-		cp.add(tempBin) # getClasspathEntities() contains self.bin, which isn't up to date yet
-		assert 0 == tmt.java(
-						main="cs.min2phase.Tools",
-						classpath=self.toClasspath(cp),
-						args=[ tablesPath ])
-		print "Successfully generated %s!" % tablesPath
+		if src == self.src:
+			# build the pruning and transition tables for the two phase algorithm
+			tablesPath = join(tempBin, self.twophase_tables)
+			print "Generating %s" % tablesPath
+			cp = self.getClasspathEntities()
+			cp.add(tempBin) # getClasspathEntities() contains self.bin, which isn't up to date yet
+			assert 0 == tmt.java(
+							main="cs.min2phase.Tools",
+							classpath=self.toClasspath(cp),
+							args=[ tablesPath ])
+			print "Successfully generated %s!" % tablesPath
 
 Project(tmt.projectName(), description="A copy of Chen Shuang's (https://github.com/ChenShuang) awesome 3x3 scrambler built on top of Herbert Kociemba's Java library.")
