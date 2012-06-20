1f61e39ff8cd5d646eccabd83575d1cd8e2ad0f8
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-06-20 02:32:55 -0700

    Updated upload script to deal with cubing org.

--- timer/tmtproject.py
+++ timer/tmtproject.py
@@ -7,7 +7,8 @@ class Project(tmt.EclipseProject):
 		tmt.Server.addPlugin(self)
 		self.nonJavaSrcDeps += [ 'tnoodleServerHandler/timer/' ]
 
-	def compile(self):
-		recompiled = tmt.EclipseProject.compile(self)
+		# I'd rather maintain a single TNoodle.jar binary that multiple different
+		# jars with different subsets of what I consider TNoodle.
+		self.fullName = "TNoodle"
 
 Project(tmt.projectName(), description="A Rubik's Cube timer.")
