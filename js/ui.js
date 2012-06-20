0071a15ef69146d653ae93e2bd1381788b86c479
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-06-19 23:31:40 -0700

    Url pretty characters! fixes #8

--- js/ui.js
+++ js/ui.js
@@ -642,7 +642,7 @@ mark2.ui = (function() {
     }
 
 	var getScrambleSheets = function() {
-		var rounds = getRoundsJSON();
+		var rounds = getRounds();
 		var sheets = [];
 		var sheetByGuid = {};
 		for(var i = 0; i < rounds.length; i++) {
@@ -746,8 +746,8 @@ mark2.ui = (function() {
 			resetRounds();
 			addRounds(settings.default_rounds);
 		}
-		else if(roundsHash != JSON.stringify(getRoundsJSON())) {
-			var rounds = JSON.parse(roundsHash);
+		else if(roundsHash != toURLPretty(getRounds())) {
+			var rounds = parseUrlPretty(roundsHash);
 			resetRounds();
 			addRounds(rounds);
 		}
@@ -763,7 +763,7 @@ mark2.ui = (function() {
 
 	var updateHash = function() {
 		var competitionName = encodeURIComponent(getCompetitionName());
-		var roundsHash = encodeURIComponent(JSON.stringify(getRoundsJSON()));
+		var roundsHash = encodeURIComponent(toURLPretty(getRounds()));
 		location.hash = "#competition_name=" + competitionName + "&rounds=" + roundsHash;
 
 		callbacks.competitionChanged();
@@ -876,7 +876,94 @@ mark2.ui = (function() {
 		roundsTbody.innerHTML = "";
 	};
 
-    var getRoundsJSON = function() {
+	function findStringsAndSurroundWith(str, encapsulator, newEncapsulator) {
+		assert(encapsulator.length == 1);
+		assert(newEncapsulator.length == 1);
+		// Replace all strings in encapsulator with strings in newEncapsulator
+		var stringRe = new RegExp(encapsulator + "([^" + encapsulator + "\\\\]|\\\\.)*" + encapsulator, "g");
+		str = str.replace(stringRe, function(str) {
+			// Remove beginning and ending encapsulator
+			str = str.substring(1, str.length - 1);
+			// Replace unescaped newEncapsulator with escaped
+			// single quotes
+			var escapedStr = "";
+			for(var i = 0; i < str.length; i++) {
+				if(str[i] == '\\') {
+					// slurp up the escaped character as well
+					i++;
+					escapedStr += "\\" + str[i];
+					continue;
+				}
+				if(str[i] == newEncapsulator) {
+					// escape unescaped newEncapsulator
+					escapedStr += "\\" + newEncapsulator;
+				} else {
+					escapedStr += str[i];
+				}
+			}
+			return newEncapsulator + escapedStr + newEncapsulator;
+		});
+		return str;
+	};
+
+	function findNotInDoubleQuoteAndReplaceWith(str, findStr, replaceStr) {
+		assert(findStr.length == 1);
+		assert(replaceStr.length == 1);
+
+		var inString = false;
+		var newStr = "";
+		for(var i = 0; i < str.length; i++) {
+			if(inString) {
+				if(str[i] == '"') {
+					// this is the end, beautiful friend
+					inString = false;
+				} else if(str[i] == "\\") {
+					// skip over the next character, since it's escaped
+					newStr += "\\" + str[++i];
+					continue;
+				}
+				newStr += str[i];
+			} else {
+				if(str[i] == '"') {
+					inString = true;
+				} else if(str[i] == findStr) {
+					newStr += replaceStr;
+					continue;
+				}
+				newStr += str[i];
+			}
+		}
+
+		return newStr;
+	}
+
+	var parseUrlPretty = function(urlPretty) {
+		// Replace all strings in single quotes with strings in double quotes
+		var json = findStringsAndSurroundWith(urlPretty, "'", '"');
+		json = findNotInDoubleQuoteAndReplaceWith(json, "(", "{");
+		json = findNotInDoubleQuoteAndReplaceWith(json, ")", "}");
+		json = findNotInDoubleQuoteAndReplaceWith(json, "-", ":");
+
+		json = findNotInDoubleQuoteAndReplaceWith(json, "_", ",");
+		json = findNotInDoubleQuoteAndReplaceWith(json, "i", "[");
+		json = findNotInDoubleQuoteAndReplaceWith(json, "!", "]");
+
+		return JSON.parse(json);
+	};
+	var toURLPretty = function(obj) {
+		var json = JSON.stringify(obj);
+		var urlPretty = findStringsAndSurroundWith(json, '"', "'");
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, "{", "(");
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, "}", ")");
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, ":", "-");
+
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, ",", "_");
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, "[", "i");
+		urlPretty = findNotInDoubleQuoteAndReplaceWith(urlPretty, "]", "!");
+
+		return urlPretty;
+	};
+    var getRounds = function() {
 		var rounds = [];
 		var eventsTBody = roundsTbody.children;
 
