a12a477a4b761f7cd1cb228a6171724b95c960c4
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-07-08 00:30:12 -0700

    Added workaround for symlinking on windows.

--- setupGitHooks.py
+++ setupGitHooks.py
@@ -3,27 +3,8 @@
 import os
 from os.path import split, abspath, relpath, exists
 import shutil
-
-def rmtree(dir):
-	# This method recursively deletes a directory, but doesn't follow any symlinks
-	if not os.path.exists(dir) and not os.path.islink(dir):
-		# If this path doesn't exist and is not a broken symlink,
-		# then we're done!
-		return
-	def onerror(func, path, excinfo):
-		if func == os.path.islink:
-			os.unlink(path)
-		else:
-			raise excinfo[1]
-	shutil.rmtree(dir, onerror=onerror)
-
-def createSymlinkIfNotExistsOrStale(target, name):
-  if ( not os.path.exists(name) or
-      not os.path.islink(name) or
-      not os.readlink(name) == target ):
-    print "Setting up symlink %s -> %s" % ( name, target )
-    rmtree(name)
-    os.symlink(target, name)
+import setupWindowsCygwinSymlinking
+from setupWindowsCygwinSymlinking import createSymlinkIfNotExistsOrStale, rmtree
 
 def setupGitHooksIfNotSetup():
   # Check that the git hooks folder is all set up, and set it up if it isn't
