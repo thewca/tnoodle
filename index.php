447f554681dd523b031f6c8418b070cb835c72f1
Luis J. Iáñez <luis@twisttheweb.com>
2012-09-25 10:18:13 +0200

    0.1
    Copyright removed.

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ echo "<div style='color:#444;'><br><br>";
 if (!$live) echo "Looking for the <a href='http://live.".DOMAIN."'>live results</a> site?<br><br>";
 echo "WCA delegates and competitions organizers, please <a href='http://www.facebook.com/cubecomps' target=_blank>contact here</a><br><br>";
 if (!$live) echo "<iframe src='//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fcubecomps&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=234783519900971' scrolling='no' frameborder='0' style='border:none; overflow:hidden; width:450px; height:21px;' allowTransparency='true'></iframe><br><br>";
-echo "(C) by <a href='http://www.binarema.es' target=_blank>Binarema</a> · all rights reserved";
+echo "by <a href='http://www.binarema.es' target=_blank>Binarema</a>";
 echo "</div></td></tr>";
 
 echo "</td></tr><table>";
