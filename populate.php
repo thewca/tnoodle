26fc671332515d0ae03fa08e42ac52196c39e01d
Luis J. Iáñez <luis@twisttheweb.com>
2012-10-22 10:55:02 +0200

    bug fix

--- populate.php
+++ populate.php
@@ -31,14 +31,23 @@ if ($_GET["cat_id"] && $_GET["round"])
 				"WHERE $regstable.cat_id=" .$cat_id. " AND $regstable.round=" .($round-1)." AND $timestable.t1 IS NOT NULL AND $timestable.best<\"A\" ".
 				"ORDER BY $timestable.average=\"\", $timestable.average, $timestable.best";
 			$list = mysql_query($query);
-			$ncomps = mysql_num_rows($list);
+
+			// bug - the following line is excluding competitors with all DNF from the total number of competitors
+			// $ncomps = mysql_num_rows($list);
+			// fix
+			$query =
+				"SELECT COUNT(*) AS count FROM $timestable ".
+				"WHERE cat_id=" .$cat_id. " AND round=" .($round-1);
+			$ncomps = mysql_result(mysql_query($query),0,"count");
+			// fix end
+
 			if (!$ncomps) die("Cannot open this round: no one qualified");
 			$qualified = mysql_result($event,0,"r".$round."_groupsize");
 			$maxtoproceed = floor($ncomps*.75);
 			if (!$maxtoproceed) die("Cannot open this round: not enough competitors");
 			if ($maxtoproceed < $qualified)
 			{
-				$_SESSION["c_error"] = "WARNING!\\n\\nThere were $ncomps competitors in the previous round. $qualified were set to proceed, but according to regulations, only $maxtoproceed will proceed. Following rounds could also be affected by this change.";
+				$_SESSION["c_error"] = "WARNING!\\n\\nThere were $ncomps competitors in the previous round. $qualified were set to proceed, but according to regulations, only $maxtoproceed at most can proceed. Following rounds could also be affected by this change.";
 				$qualified = $maxtoproceed;
 			}
 			$classification = 0;
