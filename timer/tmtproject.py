50301a4a8c71ea1c0bebbfe04011e7b2d1e3bd03
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-10-22 13:23:22 -0700

    Added a wca target. This fixes #60.

--- timer/tmtproject.py
+++ timer/tmtproject.py
@@ -8,8 +8,4 @@ class Project(tmt.EclipseProject):
 		tmt.EclipseProject.configure(self)
 		tmt.WinstoneServer.addPlugin(self)
 
-		# I'd rather maintain a single TNoodle.jar binary that multiple different
-		# jars with different subsets of what I consider TNoodle.
-		self.fullName = "TNoodle"
-
 Project(tmt.projectName(), description="A Rubik's Cube timer.")
---
+++ wca/tmtproject.py
@@ -0,0 +1,15 @@
+import tmt
+import subprocess
+from os.path import join, exists
+import shutil
+
+class Project(tmt.EclipseProject):
+	def configure(self):
+		tmt.EclipseProject.configure(self)
+		tmt.WinstoneServer.addPlugin(self)
+
+		# I'd rather maintain a single TNoodle.jar binary that multiple different
+		# jars with different subsets of what I consider TNoodle.
+		self.fullName = "TNoodle"
+
+Project(tmt.projectName(), description="Special target for the WCA")
