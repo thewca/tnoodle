d056989c844ade48e95a30eb262ff1fbb1cbe5b3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-10-28 14:16:22 -0700

    (tnoodle)Almost everything passes linting now! There are still a couple of php heredocs, but they're going away soon.

--- inc_initdb.php
+++ inc_initdb.php
@@ -1,8 +1,7 @@
 <?
 require_once "lib.php";
 
-$query = <<<EOT
-CREATE TABLE IF NOT EXISTS $eventstable (
+$query = "CREATE TABLE IF NOT EXISTS $eventstable (
   `id` tinyint(2) NOT NULL,
   `timelimit` varchar(9) default NULL,
   `r1` tinyint(1) NOT NULL,
@@ -22,28 +21,11 @@ CREATE TABLE IF NOT EXISTS $eventstable (
   `r4_groupsize` smallint(3) NOT NULL,
   `r4_open` tinyint(1) NOT NULL,
   PRIMARY KEY  (`id`)
-)
-EOT;
-//<<< ENGINE=MyISAM AUTO_INCREMENT=44 DEFAULT CHARSET=utf8
+)";
 
 $result = strict_mysql_query($query);
 
-/*<<<
-$query = <<<EOT
-CREATE TABLE IF NOT EXISTS $compstable (
-  `id` SMALLINT( 3 ) NOT NULL AUTO_INCREMENT ,
-  `WCAid` VARCHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-  `name` VARCHAR( 80 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-  `country_id` VARCHAR( 2 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-  `birthday` DATE NOT NULL ,
-  `gender` VARCHAR( 1 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-  PRIMARY KEY ( `id` ) ,
-  INDEX ( `WCAid` , `name` )
-) TYPE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci
-EOT;
-*/
-$query = <<<EOT
-CREATE TABLE IF NOT EXISTS $compstable (
+$query = "CREATE TABLE IF NOT EXISTS $compstable (
   `id` SMALLINT( 3 ) NOT NULL AUTO_INCREMENT ,
   `WCAid` VARCHAR( 10 ) ,
   `name` VARCHAR( 80 ) ,
@@ -52,24 +34,19 @@ CREATE TABLE IF NOT EXISTS $compstable (
   `gender` VARCHAR( 1 ) ,
   PRIMARY KEY ( `id` ) ,
   INDEX ( `WCAid` , `name` )
-)
-EOT;
+)";
 
 $result = strict_mysql_query($query);
 
-$query = <<<EOT
-CREATE TABLE IF NOT EXISTS $regstable (
+$query = "CREATE TABLE IF NOT EXISTS $regstable (
   `cat_id` tinyint(2) NOT NULL,
   `round` tinyint(1) NOT NULL,
   `comp_id` smallint(3) NOT NULL,
   PRIMARY KEY  (`cat_id`,`round`,`comp_id`)
-)
-EOT;
-//<<< ENGINE=MyISAM DEFAULT CHARSET=utf8;
+)";
 $result = strict_mysql_query($query);
 
-$query = <<<EOT
-CREATE TABLE IF NOT EXISTS $timestable (
+$query = "CREATE TABLE IF NOT EXISTS $timestable (
   `cat_id` tinyint(2) NOT NULL,
   `round` tinyint(1) NOT NULL,
   `comp_id` smallint(3) NOT NULL,
@@ -81,9 +58,7 @@ CREATE TABLE IF NOT EXISTS $timestable (
   `average` varchar(15) default NULL,
   `best` varchar(15) NOT NULL,
   PRIMARY KEY  (`cat_id`,`round`,`comp_id`)
-)
-EOT;
-//<<< ENGINE=MyISAM DEFAULT CHARSET=utf8
+)";
 $result = strict_mysql_query($query);
 
 ?>
