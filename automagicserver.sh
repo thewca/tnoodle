8fc2f99303ab7d01756749f6f768690f132c9209
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-03-23 16:21:43 -0700

    (tnoodle)bash isn't in the same place everywhere.

--- automagicserver.sh
+++ automagicserver.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # From http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
