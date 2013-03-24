7b5c5b6d869ce38af4e8e3220eb37d2fa8fd4c45
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-03-23 18:39:55 -0700

    Improved automagic server script to handle multiple subprojects for a given repo!

---
+++ RunAll.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+gittools = __import__("git-tools")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--inject')
+args = parser.parse_args()
+
+startWcaCommand = 'java -jar wca/dist/TNoodle.jar -n -p 8080'
+if args.inject:
+   startWcaCommand += ' --inject %s' % args.inject
+projects = [
+   gittools.GitSensitiveProject(name='wca',
+      compileCommand='./tmt make dist -p wca',
+      runCommand=startWcaCommand),
+   gittools.GitSensitiveProject(name='noderacer',
+      compileCommand='./tmt make -p noderacer',
+      runCommand='./tmt make run -p noderacer')
+]
+gittools.startGitSensitiveScreen("tnoodle", projects)
---
+++ git-tools/__init__.py
@@ -0,0 +1,45 @@
+import subprocess
+
+class GitSensitiveProject(object):
+   def __init__(self, name, compileCommand, runCommand):
+      self.name = name
+      self.compileCommand = compileCommand
+      self.runCommand = runCommand
+
+def startGitSensitiveScreen(screenTitle, projects):
+   uniqueNames = set([ project.name for project in projects ])
+   assert len(uniqueNames) == len(projects), "Project names must be unique"
+   assert all([ ' ' not in project.name for project in projects ]), "Project names must not contain spaces"
+
+   screenrc = ""
+   for i, project in enumerate(projects):
+      screenrc += 'screen -t "%s" %s\n' % ( project.name, i+1 )
+      # savepid does some magic with "set -o monitor" that I don't fully understand,
+      # but a side effect of it is that ctrl-c and ctrl-z don't work. We sleep 1 to
+      # give people a chance to kill this loop.
+      screenrc += 'stuff "while true; do git-tools/savepid git-tools/pids/%s.pid %s; sleep 1; done\\012"\n' % ( project.name, project.runCommand )
+      screenrc += "\n"
+
+   screenrc += "\n"
+   i += 1
+   screenrc += 'screen -t "git" %s\n' % (i + 1)
+   compileCommands = " && ".join([ project.compileCommand for project in projects ])
+   killCommands = " && ".join([ 'sudo kill -- -$(ps opgid= `cat git-tools/pids/%s.pid`)' % project.name for project in projects ])
+   screenrc += """
+# Trick to kill whole process tree stolen from
+#  http://stackoverflow.com/a/15139734
+stuff "git-tools/poll.sh \\"%s && %s\\";\\012"
+""" % ( compileCommands, killCommands )
+   
+   screenrcFile = open("git-tools/pids/screenrc", "w")
+   screenrcFile.write(screenrc)
+   screenrcFile.close()
+   
+   # screen -ls always returns 1 exit status for some reason
+   screens = subprocess.check_output("screen -ls || exit 0", shell=True)
+   if screenTitle in screens:
+      # TODO if there's more than one session named screenTitle, this doesn't work
+      subprocess.check_call("screen -S %s -X quit" % screenTitle, shell=True)
+
+   print "Starting screen named %s" % screenTitle
+   subprocess.check_call("screen -d -m -S %s -c git-tools/pids/screenrc" % screenTitle, shell=True)
