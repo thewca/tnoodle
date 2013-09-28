ee50d61ae515e28e1d3ecb05e6bb619f4335f99d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-09-28 04:03:04 -0700

    OOP-ified github.py. Added release support!

--- git-tools/github.py
+++ git-tools/github.py
@@ -1,88 +1,93 @@
 #!/usr/bin/python
 
-import sys
-import os
-
-import urllib
-import urllib2
 import json
-import collections
-
 import getpass
-
+import mimetypes
 import requests
 
+connectedApis = {}
+def getApi(organization, repo):
+    key = "%s/%s" % (organization, repo)
+    if key in connectedApis:
+        return connectedApis[key]
+    api = GithubApi(organization, repo)
+    connectedApis[key] = api
+    return api
 
-def httpRequest(url, data=None, method=None, username=None, password=None):
-        #assert not (username ^ password)
-        assert username and password # TODO for now, we require credentials...
+# See "Preview mode" on http://developer.github.com/changes/2013-09-25-releases-api/
+# This should be deletable in 30 days or so.
+previewHeaders = { "Accept": "application/vnd.github.manifold-preview" }
 
-        # TODO - urllib2 isn't offering up credentials unless we get a 401
-        # the github api gives a 404 if you don't have credentials, so urllib2
-        # doesn't work. The following is a horrible hack. I intend to push
-        # something upstream to the python library...
-        passwordMgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        topLevelUrl = "https://api.github.com"
-        passwordMgr.add_password(None, topLevelUrl, username, password)
+class GithubApi(object):
+    def __init__(self, organization, repo):
+        self.baseApiUrl = 'https://api.github.com/repos/%s/%s' % ( organization, repo )
+        self.baseUrl = 'https://github.com/%s/%s' % ( organization, repo )
+        self.username = raw_input('Username (for %s/%s): ' % (organization, repo))
+        print "Attempting to connect to github as %s" % ( self.username )
+        self.password = getpass.getpass()
+        self.auth = (self.username, self.password)
+        r = requests.get('https://api.github.com/user', auth=self.auth)
+        r.raise_for_status()
 
-        handler = urllib2.HTTPBasicAuthHandler(passwordMgr)
-        opener = urllib2.build_opener(handler)
+    def listReleases(self):
+        listUrl = '%s/releases' % self.baseApiUrl
+        r = requests.get(listUrl, auth=self.auth, headers=previewHeaders)
+        r.raise_for_status()
+        return r.json
 
-        req = urllib2.Request(url, data)
-        if method is not None:
-                req.get_method = lambda: method
-        try:
-                #response = urllib2.urlopen(req)
-                #handler.http_error_auth_reqed('www-authenticate', req.get_full_url(), req, None)
-                req.timeout = 5 # TODO wtf?!
-                responseHttp = handler.retry_http_basic_auth(req.get_full_url(), req, "GitHub")
-                #responseJson = json.loads(responseHttp.read())
-                return responseHttp.read()
-        except urllib2.HTTPError, e:
-                print e.headers
-                #responseJson = json.loads(e.read())
-                return e.read()
-
-def githubListFiles():
-        listUrl = '%s/downloads' % ( baseApiUrl )
-        responseText = httpRequest(listUrl, username=username, password=password)
-        responseJson = json.loads(responseText)
-        return responseJson
-
-
-def githubDeleteFileById(fileId):
-        deleteUrl = '%s/downloads/%d' % ( baseApiUrl, fileId )
-        responseText = httpRequest(deleteUrl, method="DELETE", username=username, password=password)
-        # TODO - check for errors!
-        print responseText
-
-
-def connect(organization, repo):
-        # TODO - oopify library!
-        global username, password, baseApiUrl, baseUrl
-        baseApiUrl = 'https://api.github.com/repos/%s/%s' % ( organization, repo )
-        baseUrl = 'https://github.com/%s/%s' % ( organization, repo )
-        username = raw_input('Username: ')
-        print "Attempting to connect to github as %s" % ( username )
-        password = getpass.getpass()
-        r = requests.get('https://api.github.com/user', auth=(username, password))
-        assert r.status_code == requests.codes.ok
-
-def pullRequest(title, body):
-        pullUrl = '%s/pulls' % baseApiUrl
+    def createRelease(self, tag, draft=True, files={}):
+        createUrl = '%s/releases' % self.baseApiUrl
         data = json.dumps({
-                "title": title,
-                "body": body,
-                "base": "master",
-                "head": "%s:master" % username
+            'tag_name': tag,
+            'draft': draft
         })
-        r = requests.post(pullUrl, auth=(username, password), data=data)
-        if r.status_code != requests.codes.created:
-                print r.json
-                assert r.status_code == requests.codes.created
-        else:
-                pullRequestUrl = '%s/pull/%s' % (baseUrl, r.json['number'])
-                print "Pull request created at %s" % pullRequestUrl
+        r = requests.post(createUrl, data=data, auth=self.auth, headers=previewHeaders)
+        r.raise_for_status()
 
-if __name__ == "__main__":
-        upload('TODO')
+        ogUploadUrl = r.json['upload_url']
+        fileRequests = []
+        for name, data in files.iteritems():
+            uploadUrl = ogUploadUrl.replace("{?name}", "?name=%s" % name)
+            mime, encoding = mimetypes.guess_type(name)
+            headers = dict(previewHeaders)
+            headers['Content-Type'] = mime
+
+            # Well, this is probably a huge security hole.
+            # I've contacted github about fixing their certificate. --jeremy
+            # Look at the difference between
+            #  openssl s_client -connect api.github.com:443 2>&1 | openssl x509 -text | grep -B 1 DNS
+            # and
+            #  openssl s_client -connect uploads.github.com:443 2>&1 | openssl x509 -text | grep -B 1 DNS
+            verify = False
+
+            fileRequest = requests.post(uploadUrl, auth=self.auth, headers=headers, data=data, verify=verify)
+            fileRequest.raise_for_status()
+            fileRequests.append(fileRequest.json)
+        return ( r.json, fileRequests )
+
+    def deleteRelease(self, id):
+        deleteUrl = '%s/releases/%s' % (self.baseApiUrl, id)
+        r = requests.delete(deleteUrl, auth=self.auth, headers=previewHeaders)
+        r.raise_for_status()
+
+    def listAssets(self, releaseId):
+        listUrl = '%s/releases/%s/assets' % (self.baseApiUrl, releaseId)
+        r = requests.get(listUrl, auth=self.auth, headers=previewHeaders)
+        r.raise_for_status()
+        return r.json
+
+    def pullRequest(self, title, body):
+        pullUrl = '%s/pulls' % self.baseApiUrl
+        data = json.dumps({
+            "title": title,
+            "body": body,
+            "base": "master",
+            "head": "%s:master" % self.username
+        })
+        r = requests.post(pullUrl, auth=self.auth, data=data)
+        if r.status_code != requests.codes.created:
+            print r.json
+            r.raise_for_status()
+        else:
+            pullRequestUrl = '%s/pull/%s' % (self.baseUrl, r.json['number'])
+            print "Pull request created at %s" % pullRequestUrl
