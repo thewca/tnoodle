3fc22e3b40efe3b33cfd6cf65f446586b57c02a3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-10-01 17:17:14 -0700

    Test removing large file upload workaround.

--- git-tools/github.py
+++ git-tools/github.py
@@ -93,12 +93,8 @@ class GithubApi(object):
         headers['Content-Type'] = mime
 
         r = requests.post(uploadUrl, auth=self.auth, headers=headers, data=data)
-        # Github's api is returning 502 Bad Gateway when uploading files larger than
-        # 3430400 bytes, even though the file does appear to be uploaded correctly.
-        # For now, we'll just skip this check. Hopefully we can add
-        # it back soon.
-        # r.raise_for_status()
-        # return r.json()
+        r.raise_for_status()
+        return r.json()
 
     def deleteRelease(self, id):
         deleteUrl = '%s/releases/%s' % (self.baseApiUrl, id)
