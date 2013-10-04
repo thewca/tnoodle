e2b228d2a492824ac64e9d7a0b188795708c235d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-10-04 00:37:34 -0700

    bld.html is now a target of the timer dist.

--- timer/tmtproject.py
+++ timer/tmtproject.py
@@ -1,32 +1,35 @@
 import tmt
 import unifyhtml
 import os
+from os.path import join
 
 class Project(tmt.EclipseProject):
     def configure(self):
         tmt.EclipseProject.configure(self)
         tmt.WinstoneServer.addPlugin(self)
 
-    def distUnifiedHtmlFile(self):
-        return os.path.join(self.distDir, 'tnt.html')
+        self.unifiedHtmlFiles = {
+            join(self.distDir, "tmt.html"): "http://localhost:8080/tnt/",
+            join(self.distDir, "bld.html"): "http://localhost:8080/tnt/bld.html"
+        }
 
     def dist(self, noRemake=False, implementationTitle=None):
         tmt.EclipseProject.dist(self, noRemake=noRemake, implementationTitle=implementationTitle)
 
-        # Build a standalone tnt.html
+        # Build standalone html files
         #  - Start a tnt server in the background.
-        #  - Then run unifyhtml.unify on http://localhost:8080/tnt/.
+        #  - Then run unifyhtml.unify on everything in self.unifiedHtmlFiles
         # This is easier than trying to follow the dependencies to extract
         # js files like mootools. We just let the server do its job.
         self.run(wait=False)
-        unifiedHtml = unifyhtml.unify('http://localhost:8080/tnt/', try_count=5)
-        unifiedHtml = tmt.doTextSubstitution(unifiedHtml)
 
-        unifiedHtmlFile = self.distUnifiedHtmlFile()
-        with open(unifiedHtmlFile, 'w') as out:
-            out.write(unifiedHtml)
+        for fileName, url in self.unifiedHtmlFiles.items():
+            unifiedHtml = unifyhtml.unify(url, try_count=5)
+            unifiedHtml = tmt.doTextSubstitution(unifiedHtml)
 
-        tmt.notifyDist(unifiedHtmlFile)
+            with open(fileName, 'w') as out:
+                out.write(unifiedHtml)
 
+            tmt.notifyDist(fileName)
 
 Project(tmt.projectName(), description="A Rubik's Cube timer.")
