c82e445d6569e12b1c1b005312a6e7e0a3bca713
Lucas Garron <lucasg@gmx.de>
2013-12-05 02:34:18 -0800

    Fix git tools to compute git directory "properly".
    This is not very robust, but at least it doesn't make things worse.

--- git-tools/setupGitHooks.py
+++ git-tools/setupGitHooks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 from os.path import split, abspath, relpath, exists
 import shutil
 import setupWindowsCygwinSymlinking
@@ -8,7 +9,9 @@ from setupWindowsCygwinSymlinking import createSymlinkIfNotExistsOrStale, rmtree
 
 def setupGitHooksIfNotSetup():
   # Check that the git hooks folder is all set up, and set it up if it isn't
-  gitHooksFolder = '.git/hooks'
+
+  git_dir = subprocess.check_output(["git", "rev-parse", "--git-dir"]).decode("UTF-8").strip()
+  gitHooksFolder = git_dir + '/hooks'
 
   customHooksFolder = 'git-tools/git-hooks'
   assert exists(customHooksFolder)
