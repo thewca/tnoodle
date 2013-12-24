fd8e094d76949c94cedbe7d5fe7622f279f529e1
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-12-24 04:13:24 -0800

    Munging of rotations implemented! Yay matrices.

--- svglite/src/net/gnehzr/tnoodle/svglite/Transform.java
+++ svglite/src/net/gnehzr/tnoodle/svglite/Transform.java
@@ -1,58 +1,88 @@
 package net.gnehzr.tnoodle.svglite;
 
 public class Transform {
+    // TODO - I don't know how to pick a good value here, should it be zero?
+    private static final double NEAR_THRESHOLD = 0.000001;
 
-    private Transform preTransform;
-    private String type;
-    private double[] args;
-    private Transform(String type, double... args) {
-        this.type = type;
-        this.args = args;
+    // a-f comprise the transformation matrix:
+    //  [ a c e ]
+    //  [ b d f ]
+    //  [ 0 0 1 ]
+    private double a, b, c, d, e, f;
+    private Transform(double a, double b, double c, double d, double e, double f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
     }
 
-    public Transform() {}
+    public Transform() {
+        setToIdentity();
+    }
 
     public Transform(Transform t) {
         setTransform(t);
     }
 
-    public void concatenate(Transform t) {
-        t = new Transform(t);
-        Transform firstTransform = t;
-        while(firstTransform.preTransform != null) {
-            firstTransform = firstTransform.preTransform;
-        }
-        firstTransform.preTransform = new Transform(this);
+    public void concatenate(Transform that) {
+        Transform left = that;
+        Transform right = this;
+        double a = left.a*right.a + left.c*right.b;
+        double c = left.a*right.c + left.c*right.d;
+        double e = left.a*right.e + left.c*right.f + left.e;
+        double b = left.b*right.a + left.d*right.b;
+        double d = left.b*right.c + left.d*right.d;
+        double f = left.b*right.e + left.d*right.f + left.f;
 
-        this.preTransform = t.preTransform;
-        this.type = t.type;
-        this.args = t.args;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
     }
 
     public void setTransform(Transform t) {
-        this.type = t.type;
-        this.args = t.args;
-        if(t.preTransform != null) {
-            this.preTransform = new Transform(t.preTransform);
-        }
+        this.a = t.a;
+        this.b = t.b;
+        this.c = t.c;
+        this.d = t.d;
+        this.e = t.e;
+        this.f = t.f;
     }
 
     public void setToIdentity() {
-        this.type = null;
-        this.args = null;
+        a = d = 1;
+        c = e = b = f = 0;
+    }
+
+    private static boolean isNear(double a, double b) {
+        return -NEAR_THRESHOLD <= a - b && a - b <= NEAR_THRESHOLD;
     }
 
     public boolean isIdentity() {
-        return this.type == null && this.args == null;
+        return isNear(a, 1) && isNear(d, 1) &&
+                isNear(c, 0) && isNear(e, 0) && isNear(b, 0) && isNear(f, 0);
     }
 
     public static Transform getTranslateInstance(double tx, double ty) {
-        return new Transform("translate", tx, ty);
+        return new Transform(1, 0, 0, 1, tx, ty);
     }
 
     public static Transform getRotateInstance(double radians, double anchorx, double anchory) {
-        double degrees = Math.toDegrees(radians);
-        return new Transform("rotate", degrees, anchorx, anchory);
+        Transform trans = new Transform();
+        trans.translate(-anchorx, -anchory);
+        trans.rotate(radians);
+        trans.translate(anchorx, anchory);
+        return trans;
+    }
+
+    public static Transform getRotateInstance(double radians) {
+        double sin = Math.sin(radians);
+        double cos = Math.cos(radians);
+        return new Transform(cos, sin, -sin, cos, 0, 0);
     }
 
     public void rotate(double radians, double anchorx, double anchory) {
@@ -67,35 +97,8 @@ public class Transform {
         concatenate(Transform.getTranslateInstance(x, y));
     }
 
-    public static Transform getRotateInstance(double radians) {
-        return getRotateInstance(radians, 0, 0);
-    }
-
     public String toSvgTransform() {
-        StringBuilder sb = new StringBuilder();
-        appendSvgTransform(sb);
-        return sb.toString();
-    }
-
-    public void appendSvgTransform(StringBuilder sb) {
-        if(isIdentity()) {
-            return;
-        }
-        sb.append(type).append("(");
-        boolean spaceNeeded = false;
-        for(double arg : args) {
-            if(spaceNeeded) {
-                sb.append(" ");
-            }
-            sb.append(arg);
-            spaceNeeded = true;
-        }
-        sb.append(")");
-        // Svg transforms are applied right to left
-        if(preTransform != null) {
-            sb.append(" ");
-            preTransform.appendSvgTransform(sb);
-        }
+        return "matrix(" + a + "," + b + "," + c + "," + d + "," + e + "," + f+ ")";
     }
 
 }
