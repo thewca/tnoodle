706316d620bf8e61852644f6e2601560b1b6c9d6
cs0x7f <cs0x7f@gmail.com>
2014-09-02 10:58:21 +0800

    clean up, add comments

--- scrambles/src/puzzle/ClockPuzzle.java
+++ scrambles/src/puzzle/ClockPuzzle.java
@@ -97,7 +97,7 @@ public class ClockPuzzle extends Puzzle {
 
         int[] clk_state = ClockSolver.randomState(r);
         int[] solution = new int[18];
-        ClockSolver.Solution(clk_state, solution);
+        ClockSolver.solution(clk_state, solution);
         for (int x=0; x<9; x++) {
             int turn = solution[x];
             if (turn == 0) {
@@ -273,7 +273,7 @@ public class ClockPuzzle extends Puzzle {
                     for(int j = -1; j <= 1; j++) {
                         Transform tCopy = new Transform(t);
                         tCopy.translate(2*i*clockOuterRadius, 2*j*clockOuterRadius);
-                        
+
                         Circle clockFace = new Circle(0, 0, clockRadius);
                         clockFace.setStroke(Color.BLACK);
                         clockFace.setFill(colorScheme.get(colorString[s]+ "Clock"));
@@ -383,12 +383,15 @@ public class ClockPuzzle extends Puzzle {
     }
 
     /**
-     *   1 2 3    -3 10 -1
-     *   4 5 6    11 12 13
-     *   7 8 9    -9 14 -7
+     *   0 1 2    -2  9 -0
+     *   3 4 5    10 11 12
+     *   6 7 8    -8 13 -6
      *  (front)    (back)
      */
     public static class ClockSolver {
+        private static final int N_MOVES = 18;
+        private static final int N_HANDS = 14;
+
         static int[][] moveArr = {
             { 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},    //UR
             { 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0},    //DR
@@ -410,6 +413,9 @@ public class ClockPuzzle extends Puzzle {
             {11, 0,11, 0, 0, 0,11, 0,11, 1, 1, 1, 1, 1}     //ALL
         };
 
+        /*
+         *  Combination number. Cnk[n][k] = n!/k!/(n-k)!.
+         */
         static int[][] Cnk = new int[32][32];
 
         static {
@@ -422,9 +428,22 @@ public class ClockPuzzle extends Puzzle {
                     Cnk[i][j] = Cnk[i-1][j] + Cnk[i-1][j-1];
                 }
             }
-
         }
 
+        /*
+         *  The bit map to filt linearly dependent combinations of moves.
+         */
+        static int[] ld_list = {7695, 42588, 47187, 85158, 86697, 156568, 181700, 209201, 231778};
+
+        /*
+         *  The inverse table of the ring Z/Z12. If the value is -1, the element is not inversable.
+         */
+        static int[] inv = {-1, 1,-1,-1,-1, 5,-1, 7,-1,-1,-1,11};
+
+        /**
+         *  Index [0, C(n,k)) to all C(n,k) combinations
+         *  @return the idx_th combination, represented in bitmap
+         */
         static int select(int n, int k, int idx) {
             int r = k;
             int val = 0;
@@ -437,48 +456,42 @@ public class ClockPuzzle extends Puzzle {
             return val;
         }
 
-        //invert table          0  1  2  3  4  5  6  7  8  9 10 11
-        static int[] invert = {-1, 1,-1,-1,-1, 5,-1, 7,-1,-1,-1,11};
-
         public static int[] randomState(java.util.Random r) {
-            int[] ret = new int[14];
-            for (int i=0; i<14; i++) {
+            int[] ret = new int[N_HANDS];
+            for (int i=0; i<N_HANDS; i++) {
                 ret[i] = r.nextInt(12);
             }
             return ret;
         }
 
         /**
-         *
+         *  @param hands
+         *      The 14 hands of the clock. See the comment of the class.
          *  @return the length of the solution (the number of non-zero elements in the solution array)
          *      -1: invalid input
          */
-        public static int Solution(int[] clock, int[] solution) {
-            if (clock.length != 14 || solution.length != 18) {
+        public static int solution(int[] hands, int[] solution) {
+            if (hands.length != N_HANDS || solution.length != N_MOVES) {
                 return -1;
             }
-            int ret = solveIn(14, clock, solution);
+            int ret = enumAllComb(N_HANDS, hands, solution);
             return ret;
         }
 
-        static void swap(int[][] arr, int row1, int row2) {
-            int[] tmp = arr[row1];
-            arr[row1] = arr[row2];
-            arr[row2] = tmp;
-        }
-
-        static void addTo(int[][] arr, int row1, int row2, int startidx, int mul) {
-            int length = arr[0].length;
-            for (int i=startidx; i<length; i++) {
-                arr[row2][i] = (arr[row2][i] + arr[row1][i] * mul) % 12;
-            }
-        }
-
-        //linearly dependent
-        static int[] ld_list = {7695, 42588, 47187, 85158, 86697, 156568, 181700, 209201, 231778};
-
-        static int solveIn(int k, int[] numbers, int[] solution) {
-            int n = 18;
+        /**
+         *  Enumerate all k combinations from all 18 possible moves.
+         *  For each linearly independent combination, use Gaussian Elimination to solve the clock
+         *  @param k
+         *      The number of moves, if k > 14, the selected k moves are always linearly dependent.
+         *  @param hands
+         *      The 14 hands of the clock. See the comment of the class.
+         *  @param solution
+         *      The shortest solution is stored in this parameter.
+         *  @return the length of the shortest solution, which is equal to the number of non-zero element in the solution array.
+         *      -1: the clock cannot be solved in k moves.
+         */
+        static int enumAllComb(int k, int[] hands, int[] solution) {
+            int n = N_MOVES;
             int min_nz = k+1;
 
             for (int idx=0; idx<Cnk[n][k]; idx++) {
@@ -500,19 +513,22 @@ public class ClockPuzzle extends Puzzle {
                         map[cnt++] = j;
                     }
                 }
-                int[][] arr = new int[14][k+1];
-                for (int i=0; i<14; i++) {
+                int[][] arr = new int[N_HANDS][k+1];
+                for (int i=0; i<N_HANDS; i++) {
                     for (int j=0; j<k; j++) {
                         arr[i][j] = moveArr[map[j]][i];
                     }
-                    arr[i][k] = numbers[i];
+                    arr[i][k] = hands[i];
                 }
-                int ret = GaussianElimination(arr);
+                int ret = gaussianElimination(arr);
                 if (ret != 0) {
                     continue;
                 }
+
+                //Check the rank of the coefficient matrix equal to that of the augmented matrix.
+                //If not, the clock cannot be solved by the selected moves.
                 boolean isSolved = true;
-                for (int i=k; i<14; i++) {
+                for (int i=k; i<N_HANDS; i++) {
                     if (arr[i][k] != 0) {
                         isSolved = false;
                         break;
@@ -529,7 +545,7 @@ public class ClockPuzzle extends Puzzle {
                     }
                 }
                 if (cnt_nz < min_nz) {
-                    for (int i=0; i<18; i++) {
+                    for (int i=0; i<N_MOVES; i++) {
                         solution[i] = 0;
                     }
                     for (int i=0; i<k; i++) {
@@ -541,23 +557,33 @@ public class ClockPuzzle extends Puzzle {
             return min_nz == k+1 ? -1 : min_nz;
         }
 
-        static int GaussianElimination(int[][] arr) {
-            int m = 14;
+        /**
+         *  Gaussian Elimination over the ring Z/Z12.
+         *  @return 0 if success, n if the algorithm exited at n-th step, which means the row vectors are linearly dependent.
+         */
+        static int gaussianElimination(int[][] arr) {
+            int m = N_HANDS;
             int n = arr[0].length;
             for (int i=0; i<n-1; i++) {
-                if (invert[arr[i][i]] == -1) {
+
+                //If arr[i][i] is not inversable, select or generate an inversable element in i-th column, and swap it to the i-th row.
+                if (inv[arr[i][i]] == -1) {
                     int ivtidx = -1;
+
                     for (int j=i+1; j<m; j++) {
-                        if (invert[arr[j][i]] != -1) {
+                        if (inv[arr[j][i]] != -1) {
                             ivtidx = j;
                             break;
                         }
                     }
                     if (ivtidx == -1) {
+                        //If all elements in i-th column is inversable, we will try to find two elements x, y.
+                        //If the ideal generated by {x, y} == Z/Z12, then we can generate an inversable element in i-th column.
+                        //Luckly, in Z/Z12, the ideal generated by two inversable element {x, y} == Z/Z12 is equivalent to the inversablility of x+y.
                         OUT:
                         for (int j1=i; j1<m-1; j1++) {
                             for (int j2=j1+1; j2<m; j2++) {
-                                if (invert[(arr[j1][i] + arr[j2][i]) % 12] != -1) {
+                                if (inv[(arr[j1][i] + arr[j2][i]) % 12] != -1) {
                                     addTo(arr, j2, j1, i, 1);
                                     ivtidx = j1;
                                     break OUT;
@@ -567,17 +593,15 @@ public class ClockPuzzle extends Puzzle {
                     }
                     if (ivtidx == -1) { //k vectors are linearly dependent
                         for (int j=i+1; j<m; j++) {
-                            if (arr[j][i] != 0) {
-                                return -1;
-                            }
+                            assert(arr[j][i] == 0);
                         }
                         return i + 1;
                     }
                     swap(arr, i, ivtidx);
                 }
-                int inv = invert[arr[i][i]];
+                int invVal = inv[arr[i][i]];
                 for (int j=i; j<n; j++) {
-                    arr[i][j] = arr[i][j] * inv % 12;
+                    arr[i][j] = arr[i][j] * invVal % 12;
                 }
                 for (int j=i+1; j<m; j++) {
                     addTo(arr, i, j, i, 12 - arr[j][i]);
@@ -587,7 +611,6 @@ public class ClockPuzzle extends Puzzle {
         }
 
         static void backSubstitution(int[][] arr) {
-            int m = 14;
             int n = arr[0].length;
             for (int i=n-2; i>0; i--) {
                 for (int j=i-1; j>=0; j--) {
@@ -595,6 +618,22 @@ public class ClockPuzzle extends Puzzle {
                         addTo(arr, i, j, i, 12 - arr[j][i]);
                     }
                 }
+            }
+        }
+
+        static void swap(int[][] arr, int row1, int row2) {
+            int[] tmp = arr[row1];
+            arr[row1] = arr[row2];
+            arr[row2] = tmp;
+        }
+
+        /**
+         *  arr[row2][startidx:end] += arr[row1][startidx:end] * mul
+         */
+        static void addTo(int[][] arr, int row1, int row2, int startidx, int mul) {
+            int length = arr[0].length;
+            for (int i=startidx; i<length; i++) {
+                arr[row2][i] = (arr[row2][i] + arr[row1][i] * mul) % 12;
             }
         }
     }
