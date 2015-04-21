c132d7fcd27fb0288602ef3e40826e12baf71e60
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-04-21 14:34:05 -0700

    Tweaked build to actually include the ttf files.

--- webscrambles/tmtproject.py
+++ webscrambles/tmtproject.py
@@ -7,7 +7,6 @@ class Project(tmt.EclipseProject):
         tmt.WinstoneServer.addPlugin(self)
 
         self.nonJavaResourceDeps |= tmt.glob(self.src, '.*html$', relativeTo=self.src)
-        self.nonJavaResourceDeps |= tmt.glob(self.src, '.*ttf$', relativeTo=self.src)
-        print(self.nonJavaResourceDeps)
+        self.nonJavaResourceDeps |= tmt.glob(self.srcResource, '.*ttf$', relativeTo=self.srcResource)
 
 Project(tmt.projectName(), description="A server plugin wrapper for scrambles that also draws pdfs.")
