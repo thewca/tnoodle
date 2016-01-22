e8c739dc238ddaedcd2e2e2d03e1b04a827f294e
James <James.G.Molloy@gmail.com>
2016-01-22 01:15:59 +0000

    Update SquareOnePuzzle.java
    Revert change to standard BOY scheme

--- scrambles/src/puzzle/SquareOnePuzzle.java
+++ scrambles/src/puzzle/SquareOnePuzzle.java
@@ -55,9 +55,7 @@ public class SquareOnePuzzle extends Puzzle {
         defaultColorScheme.put("B", new Color(0xffc800));
         defaultColorScheme.put("R", new Color(0x00ff00));
         defaultColorScheme.put("F", new Color(0xff0000));
-        defaultColorScheme.put("U", new Color(0xe6b800));
-        // Different yellow used compared to other BOY schemes
-        // Yellow/White are always top/bottom in square-1 so increased contrast is beneficial for reasons
+        defaultColorScheme.put("U", new Color(0xffff00));
         defaultColorScheme.put("D", new Color(0xffffff));
     }
     @Override
