c0ab08b399d3594d277a79a6384d8be1b12f5a7f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-11-14 18:25:27 -0800

    Update to support latest WCIF version.

--- tnoodle-ui/src/Home.js
+++ tnoodle-ui/src/Home.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { toWcaUrl } from 'WcaApi';
 
 import Layout from 'Layout';
 import * as actions from 'actions';
 
 class SelectCompetition extends Component {
   render() {
+    let competitions = this.props.competitions;
     return (
       <div>
         <h2>Select a competition to manage</h2>
-        <ul>
-          {this.props.competitions.map(competition => {
-            return (
-              <li key={competition.id}>
-                <Link to={`/competitions/${competition.id}`}>{competition.id}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        {competitions.length === 0 ? (
+          <span>
+            No competitions found, are you sure you have upcoming competitions?
+            Try checking <a href={toWcaUrl("/competitions/mine")} target="_blank">on the WCA website</a>.
+          </span>
+        ) : (
+          <ul>
+            {competitions.map(competition => {
+              return (
+                <li key={competition.id}>
+                  <Link to={`/competitions/${competition.id}`}>{competition.id}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     );
   }
