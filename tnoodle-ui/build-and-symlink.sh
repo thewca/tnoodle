a5bec87307a181201c76e72aaf3f58ddc0e78c8e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-12-20 00:02:28 -0800

    realpath doesn't exist on travis, so just use python.

--- tnoodle-ui/build-and-symlink.sh
+++ tnoodle-ui/build-and-symlink.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Copied from https://stackoverflow.com/a/7305217/1739415
+relpath(){ python3 -c "import os.path; print(os.path.relpath('$1', start='${2:-$PWD}'))" ; }
+
 cd "$(dirname "$0")"
 
 yarn install
@@ -14,6 +17,6 @@ for file in `find static` index.html; do
         name=$base_dir/$file
         mkdir -p `dirname $name`
         name_parent=`dirname $name`
-        ln -s `realpath --relative-to="$name_parent" "$file"` $name
+        ln -s `relpath "$file" "$name_parent"` $name
     fi
 done
