b687cdae8aa28b1263b8ead576591aeb537c884e
Tim Reynolds <tim@cubingusa.org>
2018-10-07 13:07:47 -0400

    Replace NotImplementedException in winstone.
    The version of winstone we use uses a private API for
    NotImplementedException.  It's not accessible in openjdk 10.
    
    To work around this, replace it with a custom NotImplementedException.

--- winstone/src/winstone/jndi/resourceFactories/WinstoneConnection.java
+++ winstone/src/winstone/jndi/resourceFactories/WinstoneConnection.java
@@ -25,8 +25,6 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import winstone.Logger;
 
 /**
