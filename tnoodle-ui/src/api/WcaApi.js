46492500fb44ef5bff3b8ccbe7dc3949a2a9b1a0
Oleh T <olezhek_85@list.ru>
2018-10-20 23:55:36 +0300

    Created api folder and moved TNoodle and Wca apis

--- tnoodle-ui/src/api/WcaApi.js
+++ tnoodle-ui/src/api/WcaApi.js
@@ -1,18 +1,21 @@
-import { BASE_PATH } from 'App';
+import { BASE_PATH } from '../components/App';
 
 // Members of the Software Team can configure this here: https://www.worldcubeassociation.org/oauth/applications/123.
-let WCA_ORIGIN = process.env.REACT_APP_WCA_ORIGIN || 'https://www.worldcubeassociation.org';
-let TNOODLE_APP_ID = process.env.REACT_APP_TNOODLE_APP_ID || '6145bf3e65fbad4715b049dae2d72a64b8e9a794010abf518fa9364b05a5dd40';
+let WCA_ORIGIN =
+  process.env.REACT_APP_WCA_ORIGIN || 'https://www.worldcubeassociation.org';
+let TNOODLE_APP_ID =
+  process.env.REACT_APP_TNOODLE_APP_ID ||
+  '6145bf3e65fbad4715b049dae2d72a64b8e9a794010abf518fa9364b05a5dd40';
 
-if(isUsingStaging()) {
+if (isUsingStaging()) {
   // See https://github.com/thewca/worldcubeassociation.org/wiki/OAuth-documentation-notes#staging-oauth-application
-  WCA_ORIGIN = "https://staging.worldcubeassociation.org";
-  TNOODLE_APP_ID = "example-application-id";
+  WCA_ORIGIN = 'https://staging.worldcubeassociation.org';
+  TNOODLE_APP_ID = 'example-application-id';
 }
 
 let wcaAccessToken = getHashParameter('access_token');
-if(wcaAccessToken) {
-  window.location.hash = "";
+if (wcaAccessToken) {
+  window.location.hash = '';
   localStorage['TNoodle.accessToken'] = wcaAccessToken;
   gotoPreLoginPath();
 } else {
@@ -29,7 +32,9 @@ export function toWcaUrl(path) {
 
 export function logIn() {
   let redirectUri = window.location.origin + BASE_PATH + '/oauth/wca';
-  let logInUrl = toWcaUrl(`/oauth/authorize?client_id=${TNOODLE_APP_ID}&redirect_uri=${redirectUri}&response_type=token&scope=public+manage_competitions`);
+  let logInUrl = toWcaUrl(
+    `/oauth/authorize?client_id=${TNOODLE_APP_ID}&redirect_uri=${redirectUri}&response_type=token&scope=public+manage_competitions`
+  );
   localStorage['TNoodle.preLoginHref'] = window.location.href;
   window.location = logInUrl;
 }
@@ -41,21 +46,25 @@ export function logOut() {
 }
 
 export function gotoPreLoginPath() {
-  let preLoginHref = localStorage['TNoodle.preLoginHref'] || "/";
+  let preLoginHref = localStorage['TNoodle.preLoginHref'] || '/';
   delete localStorage['TNoodle.preLoginHref'];
   window.location.replace(preLoginHref);
 }
 
 export function me() {
-  return wcaApiFetch("/me").then(response => response.json()).then(json => json.me);
+  return wcaApiFetch('/me')
+    .then(response => response.json())
+    .then(json => json.me);
 }
 
 export function getCompetitionJson(competitionId) {
-  return wcaApiFetch(`/competitions/${competitionId}/wcif`).then(response => response.json());
+  return wcaApiFetch(`/competitions/${competitionId}/wcif`).then(response =>
+    response.json()
+  );
 }
 
 export function getUpcomingManageableCompetitions() {
-  let oneWeekAgo = new Date(Date.now() - 7*24*60*60*1000);
+  let oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
   return wcaApiFetch(
     `/competitions?managed_by_me=true&start=${oneWeekAgo.toISOString()}`
   ).then(response => response.json());
@@ -78,7 +87,7 @@ function parseQueryString(query) {
   return (/^[?#]/.test(query) ? query.slice(1) : query)
     .split('&')
     .reduce((params, param) => {
-      let [ key, value ] = param.split('=');
+      let [key, value] = param.split('=');
       params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
       return params;
     }, {});
@@ -86,16 +95,16 @@ function parseQueryString(query) {
 
 function wcaApiFetch(path, fetchOptions) {
   // TODO - look into refresh token https://github.com/doorkeeper-gem/doorkeeper/wiki/Enable-Refresh-Token-Credentials
-  var baseApiUrl = toWcaUrl("/api/v0");
+  var baseApiUrl = toWcaUrl('/api/v0');
   fetchOptions = Object.assign({}, fetchOptions, {
     headers: new Headers({
-      "Authorization": `Bearer ${wcaAccessToken}`,
-      "Content-Type": "application/json",
-    }),
+      Authorization: `Bearer ${wcaAccessToken}`,
+      'Content-Type': 'application/json'
+    })
   });
 
   return fetch(`${baseApiUrl}${path}`, fetchOptions).then(response => {
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error(`${response.status}: ${response.statusText}`);
     }
     return response;
