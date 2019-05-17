b7deb085323fea9ec14bbe40f485eb9d49681624
Gregor Billing <gregor.billing@freiheit.com>
2019-05-17 15:31:54 +0200

    Include Quercus as self-contained dependency

--- quercus/build.gradle.kts
+++ quercus/build.gradle.kts
@@ -1,5 +1,19 @@
 plugins {
-    war
+    `java-library`
 }
 
 description = "A 100% java implementation of php."
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    runtime("com.h2database:h2:1.4.199")
+    runtime("com.caucho:quercus:4.0.60")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
