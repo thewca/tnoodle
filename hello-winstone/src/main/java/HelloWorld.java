b95ae32c26a46427d872f2435765c97a91bf7315
Gregor Billing <gregor.billing@freiheit.com>
2019-05-17 15:31:29 +0200

    Update hello-winstone project structure

--- hello-winstone/src/main/java/HelloWorld.java
+++ hello-winstone/src/main/java/HelloWorld.java
@@ -1,9 +1,12 @@
-
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
@@ -11,20 +14,27 @@ import javax.sql.DataSource;
 import net.gnehzr.tnoodle.server.SafeHttpServlet;
 
 @SuppressWarnings("serial")
+@WebServlet(name = "Hello", urlPatterns = {"/HelloWorld"}, loadOnStartup = 1)
 public class HelloWorld extends SafeHttpServlet {
-
-    public HelloWorld() {}
-
-    protected void wrappedService(HttpServletRequest request, HttpServletResponse response, String[] path, java.util.LinkedHashMap<String,String> query) throws Exception {
-        PrintWriter out = response.getWriter();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
         out.println("Hello World");
 
-        InitialContext jndiContext = new InitialContext();
-        DataSource ds = (DataSource) jndiContext.lookup("java:comp/env/jdbc/connPool");
-        Connection conn = ds.getConnection("root", "password");
-        Statement stmt = conn.createStatement();
-        boolean results = stmt.execute("SELECT NOW();");
-        out.println("<br>results: " + results + "<br>");
-    }
+        try {
+            InitialContext jndiContext = new InitialContext();
+            DataSource ds = (DataSource) jndiContext.lookup("java:comp/env/jdbc/connPool");
 
+            Connection conn = ds.getConnection("root", "password");
+            Statement stmt = conn.createStatement();
+
+            ResultSet results = stmt.executeQuery("SELECT NOW() AS time;");
+
+            if (results.next()) {
+                out.println("<br>results: " + results.getString("time") + "<br>");
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
 }
