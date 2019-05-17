6a964a7fa531273ac9d7fd08a55406c27f708be1
Gregor Billing <gregor.billing@freiheit.com>
2019-05-17 15:44:48 +0200

    Restructure some utils

--- web-utils/src/main/java/net/gnehzr/tnoodle/server/SafeHttpServlet.java
+++ web-utils/src/main/java/net/gnehzr/tnoodle/server/SafeHttpServlet.java
@@ -5,80 +5,55 @@ import static net.gnehzr.tnoodle.utils.GsonUtils.GSON;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Scanner;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.gnehzr.tnoodle.utils.Utils;
 import net.gnehzr.tnoodle.utils.GwtSafeUtils;
-import com.petebevin.markdown.MarkdownProcessor;
+
+import org.markdownj.MarkdownProcessor;
 
 @SuppressWarnings("serial")
 public abstract class SafeHttpServlet extends HttpServlet {
     private static final Logger l = Logger.getLogger(SafeHttpServlet.class.getName());
 
     @Override
-    protected final void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LinkedHashMap<String, String> query = parseQuery(request.getQueryString());
-        String pathInfo = request.getPathInfo();
-        String[] path;
-        if(pathInfo != null) {
-            path = pathInfo.substring(1).split("/"); // skip leading /
-        } else {
-            path = new String[0];
-        }
-
+    protected final void service(HttpServletRequest request, HttpServletResponse response) {
         try {
             response.setHeader("Access-Control-Allow-Origin", "*"); //this allows x-domain ajax
-            wrappedService(request, response, path, query);
-        } catch(Throwable e) {
+            super.service(request, response);
+        } catch (Throwable e) {
             sendError(request, response, e);
         }
     }
 
     public static String getCompletePath(HttpServletRequest request) {
         String path = request.getServletPath();
-        if(request.getPathInfo() != null) {
-            path += request.getPathInfo();
+
+        if (request.getPathInfo() != null) {
+            return path + request.getPathInfo();
         }
+
         return path;
     }
 
     public static String getExtension(HttpServletRequest request) {
-        String[] filename_ext = GwtSafeUtils.parseExtension(getCompletePath(request));
-        return filename_ext[1];
+        String[] filenameExt = GwtSafeUtils.parseExtension(getCompletePath(request));
+
+        return filenameExt[1];
     }
 
-    protected abstract void wrappedService(HttpServletRequest request, HttpServletResponse response, String[] path, LinkedHashMap<String, String> query) throws Exception;
+    public static String[] parsePath(String pathInfo) {
+        return pathInfo != null ? pathInfo.substring(1).split("/") : new String[0];
+    }
 
-    public static LinkedHashMap<String, String> parseQuery(String query) {
-        LinkedHashMap<String, String> queryMap = new LinkedHashMap<String, String>();
-        if(query == null) {
-            return queryMap;
-        }
-        String[] pairs = query.split("&");
-        for(String pair : pairs) {
-            String[] key_value = pair.split("=");
-            if(key_value.length == 1) {
-                queryMap.put(key_value[0], ""); // this allows for flags such as http://foo/blah?kill&burn
-            } else {
-                try {
-                    queryMap.put(key_value[0], URLDecoder.decode(key_value[1], "utf-8"));
-                } catch (UnsupportedEncodingException e) {
-                    queryMap.put(key_value[0], key_value[1]); //worst case, just put the undecoded string
-                }
-            }
-        }
-        return queryMap;
+    public static HashMap<String, String> parseQuery(String query) {
+        return Utils.parseQuery(query);
     }
 
     protected static void sendJS(HttpServletRequest request, HttpServletResponse response, String js) {
@@ -94,20 +69,23 @@ public abstract class SafeHttpServlet extends HttpServlet {
         // .json, our catch(Throwable e) {...} in handle() above will wrap up exceptions
         // as text, and we'll respond to a request that expects JSON with plaintext.
         azzert("json".equals(ext), "Attempted to respond with JSON to a url not ending in .json.");
-        if(callback != null) {
+
+        if (callback != null) {
             json = callback + "(" + json + ")";
         }
+
         sendBytes(request, response, json.getBytes(), "application/json"); //TODO - charset?
     }
 
     protected static void sendError(HttpServletRequest request, HttpServletResponse response, Throwable error) {
         sendError(request, response, Utils.throwableToString(error));
     }
+
     protected static void sendError(HttpServletRequest request, HttpServletResponse response, String error) {
         response.setStatus(500);
 
         String extension = getExtension(request);
-        if("json".equals(extension)) {
+        if ("json".equals(extension)) {
             HashMap<String, String> json = new HashMap<String, String>();
             json.put("error", error);
             sendJSON(request, response, GSON.toJson(json));
@@ -143,6 +121,7 @@ public abstract class SafeHttpServlet extends HttpServlet {
     protected static void sendHtml(HttpServletRequest request, HttpServletResponse response, ByteArrayOutputStream bytes) {
         sendBytes(request, response, bytes, "text/html");
     }
+
     protected static void sendHtml(HttpServletRequest request, HttpServletResponse response, byte[] bytes) {
         sendBytes(request, response, bytes, "text/html");
     }
@@ -151,7 +130,7 @@ public abstract class SafeHttpServlet extends HttpServlet {
         sendBytes(request, response, text.getBytes(), "text/plain"); //TODO - encoding charset?
     }
 
-    private static final MarkdownProcessor mp = new MarkdownProcessor();
+    private static final MarkdownProcessor MP = new MarkdownProcessor();
 
     private static String markdownToHTML(String dataString) {
         String titleCode = "";
@@ -160,7 +139,7 @@ public abstract class SafeHttpServlet extends HttpServlet {
             String title = new Scanner(dataString).nextLine();
             title = title.substring(1);
             if (title.endsWith("#")) {
-                title = title.substring(0, title.length()-1);
+                title = title.substring(0, title.length() - 1);
             }
             title = title.trim();
             titleCode = "<title>" + title + "</title>\n";
@@ -169,7 +148,7 @@ public abstract class SafeHttpServlet extends HttpServlet {
         return "<html><head>\n" +
             titleCode +
             "<link href=\"/css/markdown.css\" rel=\"stylesheet\" type=\"text/css\" />\n" +
-            "</head>\n<body>\n" + mp.markdown(dataString) + "</body>\n</html>\n";
+            "</head>\n<body>\n" + MP.markdown(dataString) + "</body>\n</html>\n";
     }
 
     protected static void sendMarkdown(HttpServletRequest request, HttpServletResponse response, String markdown) {
