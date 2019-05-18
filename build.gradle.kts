85aec05705361e90fa82a57011696940e27b6beb
Gregor Billing <suushiemaniac@gmail.com>
2019-05-18 19:28:35 +0200

    Add tnoodle-android to main project build structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,20 @@
+import configurations.Languages.attachRepositories
+
 group = "org.worldcubeassociation"
 version = "1.0-SNAPSHOT"
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+
+allprojects {
+    attachRepositories()
+}
---
+++ tnoodle-android/sample/build.gradle.kts
@@ -0,0 +1,23 @@
+import com.android.build.gradle.AppExtension
+
+plugins {
+    id("com.android.application")
+}
+
+configure<AppExtension> {
+    compileSdkVersion(23)
+
+    defaultConfig {
+        minSdkVersion(8)
+        targetSdkVersion(23)
+    }
+}
+
+dependencies {
+    implementation("com.android.support:appcompat-v7:23.2.1")
+    implementation("com.android.support:support-v4:23.2.1")
+
+    implementation("com.caverock:androidsvg:1.2.1")
+
+    implementation(project(":tnoodle-android:library"))
+}
