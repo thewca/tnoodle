2c78bd1e1b127b1865aec60bbad2eb418fa23100
Gregor Billing <suushiemaniac@gmail.com>
2019-05-24 23:15:07 +0200

    Merge :scrambles artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import configurations.Languages.attachLocalRepositories
 
 allprojects {
-    group = "org.worldcubeassociation"
+    group = "org.worldcubeassociation.tnoodle"
     version = "0.14.0"
     
     attachLocalRepositories()
