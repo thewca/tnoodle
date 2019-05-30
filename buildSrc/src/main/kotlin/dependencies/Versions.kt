ccf99dd13897fbb1c78a4d232b8acee9a626d06f
Gregor Billing <suushiemaniac@gmail.com>
2019-05-30 21:52:40 +0200

    Clean up libraries in buildSrc

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -4,24 +4,29 @@ object Versions {
     val GWT = "2.5.1"
     val JUNIT_JUPITER = "5.4.2"
     val BATIK = "1.11"
+    val KOTLIN = "1.3.31"
+    val KTOR = "1.2.0"
 
     val GWTEXPORTER = GWT
-    val GSON = "2.8.5"
-    val JOPT_SIMPLE = "3.2"
     val MARKDOWNJ_CORE = "0.4"
-    val JAVAX_SERVLET_API = "4.0.1"
     val JODA_TIME = "2.10.1"
     val ZIP4J = "1.3.3"
     val ITEXTPDF = "5.5.13"
     val BATIK_TRANSCODER = BATIK
     val SNAKEYAML = "1.24"
-    val WINSTONE = "0.9.10"
     val NATIVE_TRAY_ADAPTER = "1.2-SNAPSHOT"
     val APPLEJAVAEXTENSIONS = "1.4"
-    val URLREWRITEFILTER = "5.0.0-SNAPSHOT"
     val BOUNCYCASTLE = "1.61"
     val JUNIT_JUPITER_API = JUNIT_JUPITER
     val JUNIT_JUPITER_ENGINE = JUNIT_JUPITER
+    val KOTLIN_STDLIB_JVM = KOTLIN
+    val KOTLIN_STDLIB_JS = KOTLIN
+    val KOTLIN_STDLIB_COMMON = KOTLIN
+    val KTOR_SERVER_CIO = KTOR
+    val KTOR_GSON = KTOR
+    val KTOR_SERVER_HOST_COMMON = KTOR
+    val LOGBACK_CLASSIC = "1.2.3"
+    val KOTLIN_ARGPARSER = "2.0.7"
 
     object Plugins {
         val SHADOW = "5.0.0"
