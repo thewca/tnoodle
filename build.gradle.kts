1e194c326224bfdf7114ea351d03b1f62e5f7678
Gregor Billing <suushiemaniac@gmail.com>
2019-07-14 06:21:36 +0200

    Move ProGuard task to top level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 import configurations.Languages.attachLocalRepositories
 
+import proguard.gradle.ProGuardTask
+
 buildscript {
     repositories {
         mavenCentral()
         google()
+    }
+
+    dependencies {
+        classpath("net.sf.proguard:proguard-gradle:6.1.1")
     }
 }
 
@@ -26,21 +32,55 @@ tasks.create("registerReleaseTag") {
     }
 }
 
-tasks.create<Copy>("generateOfficialRelease") {
+tasks.create<ProGuardTask>("generateOfficialRelease") {
     description = "Generate an official WCA release artifact."
     group = "WCA"
 
     val targetProject = "webscrambles"
 
+    val targetBuildDir = project(":$targetProject").buildDir
+    val targetConfigurations = project(":$targetProject").configurations
+
     dependsOn(getTasksByName("publishToMavenLocal", true))
     dependsOn("registerReleaseTag", ":$targetProject:cleanShadowJar", ":$targetProject:shadowJar")
 
-    from("$targetProject/build/libs") {
-        include("$targetProject-$version-wca.jar")
-        rename("$targetProject-$version-wca.jar", "$releasePrefix-$version.jar")
+    injars("$targetBuildDir/libs/$targetProject-$version-all.jar")
+    outjars("$releasePrefix-$version.jar")
+
+    if (JavaVersion.current().isJava9Compatible) {
+        libraryjars("${System.getProperty("java.home")}/jmods")
+    } else {
+        libraryjars("${System.getProperty("java.home")}/lib/rt.jar")
+        libraryjars("${System.getProperty("java.home")}/lib/jce.jar")
     }
 
-    into(rootDir)
+    libraryjars(targetConfigurations.findByName("runtimeClasspath")?.files)
+
+    printmapping("$buildDir/minified/proguard.map")
+    allowaccessmodification()
+    dontskipnonpubliclibraryclassmembers()
+
+    // FIXME...? Routes currently don't work in the browser when code gets obfuscated or optimised
+    dontobfuscate()
+    dontoptimize()
+
+    // cf. https://github.com/ktorio/ktor-samples/tree/master/other/proguard
+    keep("class org.worldcubeassociation.tnoodle.server.** { *; }")
+    keep("class io.ktor.server.cio.CIO { *; }")
+    keep("class kotlin.reflect.jvm.internal.** { *; }")
+    keep("class kotlin.text.RegexOption { *; }")
+
+    keepclasseswithmembernames("""class * {
+        native <methods>;
+    }""".trimIndent())
+
+    keepclasseswithmembernames("""enum * {
+        <fields>;
+        public static **[] values();
+        public static ** valueOf(java.lang.String);
+    }""")
+
+    dontwarn()
 }
 
 tasks.create<JavaExec>("startOfficialServer") {
