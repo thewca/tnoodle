839d53b6967f2c91261eb391dcb7a8119a39ab26
Gregor Billing <suushiemaniac@gmail.com>
2019-11-13 22:48:48 +0100

    Remove redundant :scrambles Gradle configuration

--- scrambles/build.gradle.kts
+++ scrambles/build.gradle.kts
@@ -52,14 +52,3 @@ publishing {
         }
     }
 }
-repositories {
-    mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
