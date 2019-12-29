c2820c34b15f070d1ef7be463673eb0d80236740
Gregor Billing <suushiemaniac@gmail.com>
2019-12-29 18:18:16 +0100

    Bump Kotlin, GoogleCloud, Zip4j

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -4,13 +4,13 @@ object Versions {
     val GWT = "2.5.1"
     val JUNIT_JUPITER = "5.4.2"
     val BATIK = "1.12"
-    val KOTLIN = "1.3.60"
+    val KOTLIN = "1.3.61"
     val KTOR = "1.2.4"
     val PROGUARD = "6.2.0"
 
     val GWTEXPORTER = GWT
     val MARKDOWNJ_CORE = "0.4"
-    val ZIP4J = "2.2.4"
+    val ZIP4J = "2.2.8"
     val ITEXTPDF = "5.5.13.1"
     val BATIK_TRANSCODER = BATIK
     val SNAKEYAML = "1.25"
@@ -30,8 +30,8 @@ object Versions {
     val KOTLIN_ARGPARSER = "2.0.7"
     val PROGUARD_GRADLE = PROGUARD
     val WCA_I18N = "0.4.3"
-    val GOOGLE_APPENGINE_GRADLE = "2.1.0"
-    val GOOGLE_CLOUD_STORAGE = "1.85.0"
+    val GOOGLE_APPENGINE_GRADLE = "2.2.0"
+    val GOOGLE_CLOUD_STORAGE = "1.102.0"
 
     object Plugins {
         val SHADOW = "5.2.0"
