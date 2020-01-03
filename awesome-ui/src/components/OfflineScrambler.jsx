e1b6dd634c73db0e0563b24d227e3bec9aa03f2b
Alexandre Campos <camposalexandreh@gmail.com>
2020-01-03 00:15:38 -0300

    Add basic layout

---
+++ awesome-ui/src/components/OfflineScrambler.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class OfflineScrambler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Offline Scrambler</h1>
+      </div>
+    );
+  }
+}
+
+export default OfflineScrambler;
---
+++ awesome-ui/src/components/OnlineScrambler.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class OnlineScrambler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Online Scrambler</h1>
+      </div>
+    );
+  }
+}
+
+export default OnlineScrambler;
