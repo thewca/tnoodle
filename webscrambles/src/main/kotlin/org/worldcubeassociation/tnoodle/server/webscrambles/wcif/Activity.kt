a2efa5dd10813d9f7b0bdc4198f778e43800224a
Gregor Billing <suushiemaniac@gmail.com>
2020-01-03 17:42:23 +0100

    Adapt date parsing to escape from timezone hell

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/Activity.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/Activity.kt
@@ -1,9 +1,8 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.wcif
 
-import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZonedDateTime
+import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFHelper.Companion.parseWCIFDateWithTimezone
 
 data class Activity(val activityCode: String, val startTime: String) {
-    fun getLocalStartTime(timeZone: ZoneId) = ZonedDateTime.of(LocalDateTime.parse(startTime, WCIFHelper.WCIF_DATE_FORMAT), timeZone)
+    fun getLocalStartTime(timeZone: ZoneId) = startTime.parseWCIFDateWithTimezone(timeZone)
 }
