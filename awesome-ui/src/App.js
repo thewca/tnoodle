cb266e3395125cd92a7e593a53e7e25fcae291a7
Alexandre Campos <camposalexandreh@gmail.com>
2020-01-05 19:20:51 -0300

    Add cubing icons on home

--- awesome-ui/src/App.js
+++ awesome-ui/src/App.js
@@ -3,34 +3,77 @@ import "./App.css";
 import Index from "./components/Index";
 import OfflineScrambler from "./components/OfflineScrambler";
 import OnlineScrambler from "./components/OnlineScrambler";
+import About from "./components/About";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  let offlineScramblerLink = "/webscrambles/offline";
+  let onlineScramblerLink = "/webscrambles/online";
+  let aboutLink = "/about";
   return (
     <Router>
       <div className="App">
-        <div className="header" id="header">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
           <div className="container">
-            <img
-              className="tnoodle-logo"
-              src={require("./assets/tnoodle_logo.svg")}
-              alt="TNoodle logo"
-            />
-            <h1 className="display-3" id="title">
-              TNoodle
-            </h1>
+            <Link className="navbar-brand" to="/">
+              <img
+                className="tnoodle-logo"
+                src={require("./assets/tnoodle_logo.svg")}
+                alt="TNoodle logo"
+              />
+            </Link>
+            <div className="">
+              <Link to="/">
+                <h1 className="display-3" id="title">
+                  TNoodle
+                </h1>
+              </Link>
+            </div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={offlineScramblerLink}>
+                    Offline Scrambler
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={onlineScramblerLink}>
+                    Online Scrambler
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={aboutLink}>
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
+        </nav>
 
         <Switch>
-          <Route path="/webscrambles/offline">
+          <Route path={offlineScramblerLink}>
             <OfflineScrambler />
           </Route>
-          <Route path="/webscrambles/online">
+          <Route path={onlineScramblerLink}>
             <OnlineScrambler />
+          </Route>
+          <Route path={aboutLink}>
+            <About />
           </Route>
           <Route path="/">
             <Index />
---
+++ awesome-ui/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
