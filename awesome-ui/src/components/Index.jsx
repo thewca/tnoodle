81c6c13e5c41219d24fc8eb3396ae562834464cd
Alexandre Campos <camposalexandreh@gmail.com>
2020-01-05 08:49:18 -0300

    Add Table Events

--- awesome-ui/src/components/Index.jsx
+++ awesome-ui/src/components/Index.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+import "./Index.css";
+
+import { Link } from "react-router-dom";
+
 class Index extends Component {
   constructor(props) {
     super(props);
@@ -10,21 +14,25 @@ class Index extends Component {
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-md-5">
+            <div className="col-md-5 scrambler-mode">
               <h2>Offline Scrambler Program</h2>
               <p>
-                Generate scrambles by selecting events, number of rounds,
-                competition name. You can use this to generate scrambles for
-                training or for official competitions.
+                Generate scrambles by manually selecting events, number of
+                rounds, competition name. You can use this to generate scrambles
+                for training or for official competitions.
               </p>
               <p>
-                <a className="btn btn-secondary" role="button">
-                  Go Offline
-                </a>
+                <Link
+                  to="webscrambles/offline"
+                  className="btn btn-secondary"
+                  role="button"
+                >
+                  Continue Offline »
+                </Link>
               </p>
             </div>
             <div className="col-md-2"></div>
-            <div className="col-md-5">
+            <div className="col-md-5 scrambler-mode">
               <h2>Online Scrambler Program</h2>
               <p>
                 This area is mostly for delegates and organisers. Syncronize
@@ -33,9 +41,13 @@ class Index extends Component {
                 and more.
               </p>
               <p>
-                <a className="btn btn-secondary" role="button">
-                  Go Online
-                </a>
+                <Link
+                  to="webscrambles/online"
+                  className="btn btn-secondary"
+                  role="button"
+                >
+                  Go Online »
+                </Link>
               </p>
             </div>
           </div>
