4abd7b3dd7ba6b314965fabeb5921fd5e75d2d06
Gregor Billing <suushiemaniac@gmail.com>
2020-01-11 10:30:55 +0100

    Clean up translations class

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/Translate.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/Translate.kt
@@ -4,11 +4,11 @@ import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
 
 import java.util.Locale
-import java.util.regex.Pattern
 
 object Translate {
     private val BASE_LOCALE = Locale.forLanguageTag("en")
-    val DEFAULT_LOCALE = System.getenv("TNOODLE_DEFAULT_LOCALE")?.let { Locale.forLanguageTag(it) } ?: BASE_LOCALE
+    val DEFAULT_LOCALE = System.getenv("TNOODLE_DEFAULT_LOCALE")
+        ?.let { Locale.forLanguageTag(it) } ?: BASE_LOCALE
 
     val TRANSLATIONS = loadTranslationResources()
 
@@ -31,82 +31,81 @@ object Translate {
         return mergedMaps ?: emptyMap()
     }
 
-    private fun getTranslationsFor(locale: Locale): Map<String, *>? {
+    private fun getTranslationsFor(locale: Locale): Map<String, *> {
         return TRANSLATIONS[locale]
             ?: error("No locale matching given locale: ${locale.toLanguageTag()} found")
     }
 
-    private fun translate(key: String, locale: Locale, azzertIfInvalid: Boolean): String? {
-        var translation: Any? = getTranslationsFor(locale)
-
+    private fun translateNullable(key: String, locale: Locale): String? {
+        val baseTranslations = getTranslationsFor(locale)
         val parts = key.split(".")
 
-        for (part in parts) {
-            if (translation !is HashMap<*, *>) {
-                if (azzertIfInvalid) {
-                    error("${locale.toLanguageTag()} couldn't find part $part of $key")
-                } else {
-                    return null
-                }
-            }
+        return descendKeys(baseTranslations, parts)
+    }
 
-            translation = translation[part]
+    private fun translateWithError(key: String, locale: Locale): String {
+        val baseTranslations = getTranslationsFor(locale)
+        val parts = key.split(".")
+
+        val translation = descendKeys(baseTranslations, parts) {
+            "${locale.toLanguageTag()} couldn't find part $it of $key"
+        } ?: error("${locale.toLanguageTag()} translation key $key not found")
+
+        return translation as? String
+            ?: error("${locale.toLanguageTag()} translation key $key is of type ${translation.javaClass}, but we were expecting String.")
+    }
+
+    private tailrec fun descendKeys(translationGroup: Map<*, *>, parts: List<String>, errorProvider: ((String) -> String)? = null): String? {
+        if (parts.isEmpty()) {
+            return null
         }
 
-        if (translation == null) {
-            if (azzertIfInvalid) {
-                error("${locale.toLanguageTag()} translation key $key not found")
-            } else {
-                return null
-            }
+        val nextPart = parts.first()
+        val remainingParts = parts.drop(1)
+
+        val nextGroup = translationGroup[nextPart]
+
+        if (remainingParts.isEmpty()) {
+            return nextGroup as? String
         }
 
-        if (translation !is String) {
-            if (azzertIfInvalid) {
-                error("${locale.toLanguageTag()} translation key $key is of type ${translation.javaClass}, but we were expecting String.")
-            } else {
-                return null
-            }
+        if (nextGroup !is Map<*, *>) {
+            return errorProvider?.invoke(nextPart)
+                ?.let { error(it) }
         }
 
-        return translation
+        return descendKeys(nextGroup, remainingParts)
     }
 
     fun translate(key: String, locale: Locale, substitutions: Map<String, String> = mapOf()): String {
         // Attempt to translate in the given locale.
-        val translation = translate(key, locale, false)
+        val translation = translateNullable(key, locale)
             // If we couldn't find a translation in the given locale, fall back to the base locale.
-            ?: translate(key, BASE_LOCALE, true)
+            ?: translateWithError(key, BASE_LOCALE)
 
         return interpolate(translation, substitutions)
     }
 
     // Interpolate translation keys in the same way Ruby on Rails does.
     // See: http://guides.rubyonrails.org/i18n.html#passing-variables-to-translations
-    private fun interpolate(interpolateMe: String?, substitutions: Map<String, String>): String {
+    private fun interpolate(interpolateMe: String, substitutions: Map<String, String>): String {
         // Copy the given Map, as we're going to mutate it later on.
         val mutateSubstitutions = substitutions.toMutableMap()
 
         // Now interpolate variables in the input string.
         //
         // Find anything that looks like %{...}, unless the percent sign is escaped as in %%{...}
-        val templatePattern = Pattern.compile("(?<!%)%\\{([^}]+)}")
-        val matcher = templatePattern.matcher(interpolateMe)
+        val pattern = Regex("(?<!%)%\\{([^}]+)}")
+        val interpolated = pattern.replace(interpolateMe) {
+            val key = it.groups[1]?.value
+                ?: error("Translation Regex broken: Group without content for $interpolateMe")
 
-        val sb = StringBuffer()
-
-        while (matcher.find()) {
-            val key = matcher.group(1)
-
-            val replacement = mutateSubstitutions.remove(key)
-                ?: error(String.format("Substitution for key: %s not found", key))
-
-            matcher.appendReplacement(sb, replacement)
+            mutateSubstitutions.remove(key)
+                ?: error("Substitution for key: $key not found")
         }
 
-        matcher.appendTail(sb)
-        assert(mutateSubstitutions.isEmpty()) { String.format("Unused substitution values: %s", mutateSubstitutions.keys) }
+        assert(mutateSubstitutions.isEmpty()) { "Unused substitution values: ${mutateSubstitutions.keys}" }
 
-        return sb.toString()
+        return interpolated
     }
 }
