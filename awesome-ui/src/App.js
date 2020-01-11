f0f5167d73f3d5a3650589341b9e8a1143109b8b
Alexandre Campos <camposalexandreh@gmail.com>
2020-01-11 19:24:56 -0300

    Events picker

--- awesome-ui/src/App.js
+++ awesome-ui/src/App.js
@@ -7,7 +7,9 @@ import About from "./components/About";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   let offlineScramblerLink = "/webscrambles/offline";
@@ -16,60 +18,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div id="navbar-wrapper">
-          <nav
-            className="navbar navbar-expand-lg navbar-dark bg-dark static-top"
-            id="navbar"
-          >
-            <div className="container">
-              <Link className="navbar-brand" to="/">
-                <img
-                  className="tnoodle-logo"
-                  src={require("./assets/tnoodle_logo.svg")}
-                  alt="TNoodle logo"
-                />
-              </Link>
-              <div className="">
-                <Link to="/">
-                  <h1 className="display-3" id="title">
-                    TNoodle
-                  </h1>
-                </Link>
-              </div>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <Link className="nav-link" to={offlineScramblerLink}>
-                      Offline Scrambler
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to={onlineScramblerLink}>
-                      Online Scrambler
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to={aboutLink}>
-                      About
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
-
+        <Navbar
+          offlineScramblerLink={offlineScramblerLink}
+          onlineScramblerLink={onlineScramblerLink}
+          aboutLink={aboutLink}
+        />
         <Switch>
           <Route path={offlineScramblerLink}>
             <OfflineScrambler />
