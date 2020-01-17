86cc0f5e46f2fccd4fe29ece88d9be6facf28a2e
Gregor Billing <suushiemaniac@gmail.com>
2020-01-17 23:08:28 +0100

    Cleanup for ZIP methods

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/zip/model/File.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/zip/model/File.kt
@@ -1,10 +1,7 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.zip.model
 
-data class File(val name: String, val content: ByteArray, val parent: Folder? = null) : ZipNode {
+data class File(val name: String, val content: ByteArray, val parent: Folder? = null) : AbstractNode(name, parent) {
     constructor(name: String, content: String, parent: Folder? = null) : this(name, content.toByteArray(), parent)
-
-    override val path: String
-        get() = parent?.let { it.path + "/" }.orEmpty() + name
 
     override fun withParent(parent: Folder) = copy(parent = parent)
 }
