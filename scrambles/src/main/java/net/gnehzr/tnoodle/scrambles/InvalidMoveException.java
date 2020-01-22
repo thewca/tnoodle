69e047386ff20fdb50333da96e2f5661dd276544
Gregor Billing <suushiemaniac@gmail.com>
2020-01-22 23:06:34 +0100

    Remove tnoodle-lib folders

--- scrambles/src/main/java/net/gnehzr/tnoodle/scrambles/InvalidMoveException.java
+++
@@ -1,8 +0,0 @@
-package net.gnehzr.tnoodle.scrambles;
-
-@SuppressWarnings("serial")
-public class InvalidMoveException extends Exception {
-    public InvalidMoveException(String move) {
-        super("Invalid move: " + move);
-    }
-}
