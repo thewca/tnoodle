89ab62314cd1c543e4ce91932dee312862ec7dc7
Gregor Billing <suushiemaniac@gmail.com>
2020-02-07 18:34:11 +0100

    Use WCIF model classes in PDF rendering

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/BaseScrambleSheet.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/BaseScrambleSheet.kt
@@ -4,10 +4,12 @@ import com.itextpdf.text.Document
 import com.itextpdf.text.Rectangle
 import com.itextpdf.text.pdf.PdfReader
 import com.itextpdf.text.pdf.PdfWriter
-import org.worldcubeassociation.tnoodle.server.webscrambles.ScrambleRequest
+import org.worldcubeassociation.tnoodle.server.webscrambles.PuzzlePlugins
+import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.Activity
+import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.Competition
 import java.io.ByteArrayOutputStream
 
-abstract class BaseScrambleSheet(val scrambleRequest: ScrambleRequest, globalTitle: String?) : BasePdfSheet<PdfWriter>(globalTitle) {
+abstract class BaseScrambleSheet(val wcif: Competition, val activity: Activity) : BasePdfSheet<PdfWriter>(wcif.name) {
     override fun openDocument() =
         Document(PAGE_SIZE, 0f, 0f, 75f, 75f).apply {
             addCreationDate()
@@ -23,6 +25,20 @@ abstract class BaseScrambleSheet(val scrambleRequest: ScrambleRequest, globalTit
             setBoxSize("art", Rectangle(36f, 54f, PAGE_SIZE.width - 36, PAGE_SIZE.height - 54))
         }
     }
+
+    protected val scrambleSet = wcif.events.asSequence()
+        .flatMap { it.rounds.asSequence() }
+        .flatMap { it.scrambleSets.asSequence() }
+        .find { it.id == activity.scrambleSetId } // FIXME WCIF if only one attempt specified, then only draw one scr (singleton list)
+        ?: error("Cannot draw PDF: Scramble set for activity $activity not found in competition ${wcif.id}")
+
+    protected val scramblingPuzzle = PuzzlePlugins.PUZZLES[activity.activityCode.eventId]?.value // FIXME WCIF bf --> ni
+        ?: error("Cannot draw PDF: Scrambler for $activity not found in plugins")
+
+    protected val currentRound = wcif.events.asSequence()
+        .flatMap { it.rounds.asSequence() }
+        .find { it.idCode.isParentOf(activity.activityCode) }
+        ?: error("Cannot draw PDF: Round for activity $activity not found in competition ${wcif.id}")
 
     override fun finalise(processedBytes: ByteArrayOutputStream, password: String?): ByteArray {
         val pdfReader = PdfReader(processedBytes.toByteArray(), password?.toByteArray())
--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Round.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Round.kt
@@ -6,4 +6,14 @@ import kotlinx.serialization.Serializable
 data class Round(val id: String, val format: String, val scrambleSetCount: Int, val scrambleSets: List<ScrambleSet>) {
     val idCode: ActivityCode
         get() = ActivityCode(id)
+
+    val expectedAttemptNum: Int
+        get() = format.toIntOrNull() ?: FORMAT_SCRAMBLE_COUNTS[format] ?: 0 // FIXME WCIF better default?
+
+    companion object {
+        private val FORMAT_SCRAMBLE_COUNTS = mapOf(
+            "a" to 5,
+            "m" to 3
+        )
+    }
 }
