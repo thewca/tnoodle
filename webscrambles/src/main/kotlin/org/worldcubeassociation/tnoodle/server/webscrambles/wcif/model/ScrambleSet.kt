4f4f497345d7ae1785bf024166271784ddb45b0b
Gregor Billing <suushiemaniac@gmail.com>
2020-02-08 00:02:15 +0100

    Add non-parallel WCIF converter mechanism

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/ScrambleSet.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/ScrambleSet.kt
@@ -3,7 +3,7 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ScrambleSet(val id: Int, val scrambles: List<Scramble>, val extraScrambles: List<Scramble>, val extensions: List<Extension>) {
+data class ScrambleSet(val id: Int, val scrambles: List<Scramble>, val extraScrambles: List<Scramble>, val extensions: List<Extension> = emptyList()) {
     val allScrambles: List<Scramble>
         get() = scrambles + extraScrambles
 
