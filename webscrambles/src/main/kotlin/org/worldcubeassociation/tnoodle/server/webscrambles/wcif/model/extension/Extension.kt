1a049f32b871b8038b5618b1712002afe7ee9383
Gregor Billing <suushiemaniac@gmail.com>
2020-02-08 18:17:32 +0100

    Mock polymorphic serializer

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/extension/Extension.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/extension/Extension.kt
@@ -4,13 +4,16 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class Extension(val id: String, val specUrl: String) {
+sealed class Extension(val specUrl: String) {
+    abstract val id: String
     abstract val data: Any
 }
 
 @Serializable
 @SerialName(FmcExtension.ID)
-data class FmcExtension(override val data: Boolean) : Extension(ID, SPEC_URL) {
+data class FmcExtension(override val data: Boolean) : Extension(SPEC_URL) {
+    override val id get() = ID
+
     companion object {
         const val ID = "org.worldcubeassociation.tnoodle.FmcFlag"
         const val SPEC_URL = "TODO"
@@ -19,7 +22,9 @@ data class FmcExtension(override val data: Boolean) : Extension(ID, SPEC_URL) {
 
 @Serializable
 @SerialName(FmcAttemptCountExtension.ID)
-data class FmcAttemptCountExtension(override val data: Int) : Extension(ID, SPEC_URL) {
+data class FmcAttemptCountExtension(override val data: Int) : Extension(SPEC_URL) {
+    override val id get() = FmcExtension.ID
+
     companion object {
         const val ID = "org.worldcubeassociation.tnoodle.FmcAttemptCount"
         const val SPEC_URL = "TODO"
@@ -28,7 +33,9 @@ data class FmcAttemptCountExtension(override val data: Int) : Extension(ID, SPEC
 
 @Serializable
 @SerialName(ExtraScrambleCountExtension.ID)
-data class ExtraScrambleCountExtension(override val data: Int) : Extension(ID, SPEC_URL) {
+data class ExtraScrambleCountExtension(override val data: Int) : Extension(SPEC_URL) {
+    override val id get() = FmcExtension.ID
+
     companion object {
         const val ID = "org.worldcubeassociation.tnoodle.ExtraScrambleCount"
         const val SPEC_URL = "TODO"
@@ -37,7 +44,9 @@ data class ExtraScrambleCountExtension(override val data: Int) : Extension(ID, S
 
 @Serializable
 @SerialName(MultiScrambleCountExtension.ID)
-data class MultiScrambleCountExtension(override val data: Int) : Extension(ID, SPEC_URL) {
+data class MultiScrambleCountExtension(override val data: Int) : Extension(SPEC_URL) {
+    override val id get() = FmcExtension.ID
+
     companion object {
         const val ID = "org.worldcubeassociation.tnoodle.MultiScrambleCount"
         const val SPEC_URL = "TODO"
