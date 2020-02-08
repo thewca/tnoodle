be032ed41780b2487f9b84f9d3daf5f05f37ae36
Gregor Billing <suushiemaniac@gmail.com>
2020-02-08 18:10:27 +0100

    Add multi-cube extension mechanism

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/WcifHandler.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/WcifHandler.kt
@@ -3,6 +3,7 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.routing
 import io.ktor.application.call
 import io.ktor.request.receiveText
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.post
 import org.worldcubeassociation.tnoodle.server.RouteHandler
@@ -20,7 +21,15 @@ object WcifHandler : RouteHandler {
                 ?: return@post call.respond("Please specify a WCIF JSON")
 
             val wcif = WCIFParser.parseComplete(wcifJson)
-            val bindings = WCIFScrambleMatcher.fillScrambleSets(wcif)
+
+            val extendedWcif = query["multi-cubes"]?.let {
+                val count = it.toIntOrNull()
+                    ?: return@post call.respondText("Not a valid number: $it")
+
+                WCIFScrambleMatcher.installMultiCount(wcif, count)
+            } ?: wcif
+
+            val bindings = WCIFScrambleMatcher.fillScrambleSets(extendedWcif)
 
             call.respond(bindings)
         }
