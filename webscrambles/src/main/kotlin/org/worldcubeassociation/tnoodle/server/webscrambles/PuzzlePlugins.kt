91fe327851484c323f0067d0669d18d4b4fc5faa
Gregor Billing <suushiemaniac@gmail.com>
2020-02-11 13:26:22 +0100

    Integrate new PuzzleRegistry

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/PuzzlePlugins.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/PuzzlePlugins.kt
@@ -1,38 +1,34 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles
 
-import org.worldcubeassociation.tnoodle.puzzle.*
 import org.worldcubeassociation.tnoodle.scrambles.Puzzle
+import org.worldcubeassociation.tnoodle.scrambles.PuzzleRegistry
 import org.worldcubeassociation.tnoodle.scrambles.ScrambleCacher
 import org.worldcubeassociation.tnoodle.server.webscrambles.server.Plugins
 import java.util.*
 
 object PuzzlePlugins {
-    private val plugins = Plugins<Puzzle>()
+    private val plugins = Plugins()
 
     val PUZZLES: SortedMap<String, Lazy<Puzzle>> by lazy { getScramblers() }
     val SCRAMBLE_CACHERS: Map<String, ScrambleCacher> get() = plugins.cachers
 
     private fun getScramblers(): SortedMap<String, Lazy<Puzzle>> {
-        plugins.register("222", "2x2x2") { TwoByTwoCubePuzzle() }
-        plugins.register("333", "3x3x3") { ThreeByThreeCubePuzzle() }
-        plugins.register("444", "4x4x4") { FourByFourCubePuzzle() }
-        //plugins.register("444fast", "4x4x4 (fast, unofficial)") { FourByFourRandomTurnsCubePuzzle() }
-        plugins.register("555", "5x5x5") { CubePuzzle(5) }
-        plugins.register("666", "6x6x6") { CubePuzzle(6) }
-        plugins.register("777", "7x7x7") { CubePuzzle(7) }
-        plugins.register("333ni", "3x3x3 no inspection") { NoInspectionThreeByThreeCubePuzzle() }
-        plugins.register("444ni", "4x4x4 no inspection") { NoInspectionFourByFourCubePuzzle() }
-        plugins.register("555ni", "5x5x5 no inspection") { NoInspectionFiveByFiveCubePuzzle() }
-        plugins.register("333fm", "3x3x3 Fewest Moves") { ThreeByThreeCubeFewestMovesPuzzle() }
-        plugins.register("pyram", "Pyraminx") { PyraminxPuzzle() }
-        plugins.register("sq1", "Square-1") { SquareOnePuzzle() }
-        //plugins.register("sq1fast", "Square-1 (fast, unofficial)") { SquareOneUnfilteredPuzzle() }
-        plugins.register("minx", "Megaminx") { MegaminxPuzzle() }
-        plugins.register("clock", "Clock") { ClockPuzzle() }
-        plugins.register("skewb", "Skewb") { SkewbPuzzle() }
+        plugins.register(PuzzleRegistry.TWO)
+        plugins.register(PuzzleRegistry.THREE)
+        plugins.register(PuzzleRegistry.FOUR)
+        plugins.register(PuzzleRegistry.FIVE)
+        plugins.register(PuzzleRegistry.SIX)
+        plugins.register(PuzzleRegistry.SEVEN)
+        plugins.register(PuzzleRegistry.THREE_NI)
+        plugins.register(PuzzleRegistry.FOUR_NI)
+        plugins.register(PuzzleRegistry.FIVE_NI)
+        plugins.register(PuzzleRegistry.THREE_FM)
+        plugins.register(PuzzleRegistry.PYRA)
+        plugins.register(PuzzleRegistry.SQ1)
+        plugins.register(PuzzleRegistry.MEGA)
+        plugins.register(PuzzleRegistry.CLOCK)
+        plugins.register(PuzzleRegistry.SKEWB)
 
         return plugins.plugins.toSortedMap(naturalOrder())
     }
-
-    fun getScramblerLongName(shortName: String) = plugins.comments[shortName]
 }
