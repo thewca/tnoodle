765a143a86acb044856fb8ae8bf69aaca7a8fc5c
Gregor Billing <suushiemaniac@gmail.com>
2020-02-12 16:12:49 +0100

    Flatten data contents dynamically

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Extension.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Extension.kt
@@ -2,26 +2,22 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model
 
 import kotlinx.serialization.*
 import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.json
 import org.worldcubeassociation.tnoodle.server.serial.JsonConfig
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.extension.ExtensionBuilder
 
 @Serializable
 data class Extension(val id: String, val specUrl: String, val data: JsonObject) {
+    val jsonObject
+        get() = JsonConfig.SERIALIZER.toJson(serializer(), this).jsonObject
+
     inline fun <reified T : ExtensionBuilder> parsedData(): T? {
         if (id !in ExtensionBuilder.REGISTERED_CHILDREN) {
             return null
         }
 
-        val mockConstruction = json {
-            "id" to id
-            "specUrl" to specUrl
+        val mockData = jsonObject.content - "data" + data.content
+        val mockExtension = JsonObject(mockData)
 
-            for ((k, v) in data) {
-                k to v
-            }
-        }
-
-        return JsonConfig.SERIALIZER.fromJson(ExtensionBuilder.serializer(), mockConstruction) as? T
+        return JsonConfig.SERIALIZER.fromJson(ExtensionBuilder.serializer(), mockExtension) as? T
     }
 }
