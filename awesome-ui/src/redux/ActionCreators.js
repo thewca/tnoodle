495abddc3eaaac8c3c13ccc68b0460b7b25a0701
Alexandre Campos <camposalexandreh@gmail.com>
2020-02-12 20:51:41 -0300

    Update wcif events

--- awesome-ui/src/redux/ActionCreators.js
+++ awesome-ui/src/redux/ActionCreators.js
@@ -9,3 +9,18 @@ export const updateWcif = wcif => ({
   type: ActionTypes.UPDATE_ME,
   payload: { wcif: wcif }
 });
+
+export const updatePassword = password => ({
+  type: ActionTypes.UPDATE_PASSWORD,
+  payload: { password: password }
+});
+
+export const updateCompetitionName = competitionName => ({
+  type: ActionTypes.UPDATE_COMPETITION_NAME,
+  payload: { competitionName: competitionName }
+});
+
+export const updateWcaEvent = wcaEvent => ({
+  type: ActionTypes.UPDATE_WCA_EVENT,
+  payload: { wcaEvent: wcaEvent }
+});
