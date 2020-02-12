dd02db081a4ce90a40c64361716e99c581654248
Gregor Billing <suushiemaniac@gmail.com>
2020-02-12 10:37:43 +0100

    J8 backwards compatibility

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/serial/LocalDateTimeSerializer.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/serial/LocalDateTimeSerializer.kt
@@ -1,15 +1,14 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.serial
 
+import org.worldcubeassociation.tnoodle.server.webscrambles.Translate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 object LocalDateTimeSerializer : SingletonStringEncoder<LocalDateTime>("LocalDateTime") {
     const val ENC_DATE_PATTERN = "MMM dd, yyyy h:m:s a"
 
     val ENC_DATE_FORMAT = DateTimeFormatter
-        .ofPattern(ENC_DATE_PATTERN)
-        .localizedBy(Locale.ENGLISH)
+        .ofPattern(ENC_DATE_PATTERN, Translate.DEFAULT_LOCALE)
 
     override fun encodeInstance(instance: LocalDateTime) = instance.format(ENC_DATE_FORMAT)
     override fun makeInstance(deserialized: String) = LocalDateTime.parse(deserialized, ENC_DATE_FORMAT)
