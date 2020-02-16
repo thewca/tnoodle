8b67bd901e648e63b1e0d11f114d83d2626bf6e5
Gregor Billing <suushiemaniac@gmail.com>
2020-02-16 17:38:56 +0100

    De-duplicate accidental ID duplication

--- server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/serial/JsonConfig.kt
+++ server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/serial/JsonConfig.kt
@@ -4,8 +4,10 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 
 object JsonConfig {
+    const val CLASS_DISCRIMINATOR = "id"
+
     val TNOODLE_DEFAULT = JsonConfiguration.Stable
-        .copy(encodeDefaults = false, strictMode = false, classDiscriminator = "id")
+        .copy(encodeDefaults = false, strictMode = false, classDiscriminator = CLASS_DISCRIMINATOR)
 
     val SERIALIZER by lazy { Json(TNOODLE_DEFAULT) }
 }
