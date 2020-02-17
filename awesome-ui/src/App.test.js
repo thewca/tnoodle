8182e9f2a6967fb54a72b3ab25b2fe7683b531dc
Gregor Billing <suushiemaniac@gmail.com>
2020-02-17 01:30:03 +0100

    Fix unstable test

--- awesome-ui/src/App.test.js
+++ awesome-ui/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./redux/Store";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders login link', () => {
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
+  const linkElement = getByText(/Log in/i);
   expect(linkElement).toBeInTheDocument();
 });
