5b38baf03a80496f2cfea703628bb6caa10b5a18
Gregor Billing <suushiemaniac@gmail.com>
2020-02-20 13:51:38 +0100

    Clean up ScrambleViewHandler

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/ScrambleViewHandler.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/ScrambleViewHandler.kt
@@ -1,9 +1,9 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.routing
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.request.receiveText
-import io.ktor.request.uri
 import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.response.respondBytes
@@ -12,6 +12,8 @@ import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.list
 import org.apache.batik.anim.dom.SVGDOMImplementation
 import org.apache.batik.transcoder.TranscoderInput
@@ -19,8 +21,7 @@ import org.apache.batik.transcoder.TranscoderOutput
 import org.apache.batik.transcoder.TranscodingHints
 import org.apache.batik.transcoder.image.ImageTranscoder
 import org.apache.batik.util.SVGConstants
-import org.worldcubeassociation.tnoodle.scrambles.PuzzleIcon
-import org.worldcubeassociation.tnoodle.scrambles.PuzzleImageInfo
+import org.worldcubeassociation.tnoodle.scrambles.Puzzle
 import org.worldcubeassociation.tnoodle.server.RouteHandler
 import org.worldcubeassociation.tnoodle.server.RouteHandler.Companion.parseQuery
 import org.worldcubeassociation.tnoodle.server.RouteHandler.Companion.splitNameAndExtension
@@ -33,9 +34,12 @@ import org.worldcubeassociation.tnoodle.server.webscrambles.serial.PuzzleImageJs
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFDataBuilder
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFScrambleMatcher
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFParser
+import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.Competition
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.Schedule
+import org.worldcubeassociation.tnoodle.svglite.Svg
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
+import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.imageio.ImageIO
 
@@ -54,117 +58,113 @@ class ScrambleViewHandler(val environmentConfig: ServerEnvironmentConfig) : Rout
         }
     }
 
+    private suspend fun ApplicationCall.withScramble(handle: suspend ApplicationCall.(Puzzle, Svg) -> Unit) {
+        val name = parameters["puzzleKey"]
+            ?: return respondText("Please specify a puzzle")
+
+        val scrambler = PuzzlePlugins.WCA_PUZZLES[name]?.scramblerWithoutCache
+            ?: return respondText("Invalid scrambler: $name")
+
+        val scramble = request.queryParameters["scramble"]
+        val colorScheme = request.queryParameters["scheme"]
+            ?.let { scrambler.parseColorScheme(it) } ?: hashMapOf()
+
+        val svg = scrambler.drawScramble(scramble, colorScheme)
+
+        handle(scrambler, svg)
+    }
+
+    private suspend fun ApplicationCall.withSheetsWCIF(handle: suspend ApplicationCall.(Competition, Map<String, String>) -> Unit) {
+        val name = parameters["competitionName"]
+            ?: return respondText("Please specify a competition title")
+
+        val body = receiveText()
+        val query = parseQuery(body)
+
+        val reqRaw = query["sheets"]
+            ?: return respondText("Please add at least one scramble sheet to this request!")
+
+        val scrambleRequests = JsonConfig.SERIALIZER.parse(ScrambleRequest.serializer().list, reqRaw)
+        val wcif = WCIFScrambleMatcher.requestsToPseudoWCIF(scrambleRequests, name)
+
+        handle(wcif, query)
+    }
+
     override fun install(router: Routing) {
-        router.route("/view") {
-            get("/{puzzleExt}") {
-                val puzzleExt = call.parameters["puzzleExt"] ?: return@get call.respondText("Please specify a puzzle")
+        router.route("view") {
+            route("{puzzleKey}") {
+                get("png") {
+                    call.withScramble { scrambler, svg ->
+                        val svgFile = svg.toString().byteInputStream()
 
-                val (name, extension) = splitNameAndExtension(puzzleExt)
+                        val (width, height) = scrambler.preferredSize.let { it.width to it.height }
+                        val imageTranscoder = BufferedImageTranscoder()
 
-                if (extension.isEmpty()) {
-                    return@get call.respondText("No extension specified.")
-                }
+                        // Copied from http://stackoverflow.com/a/6634963
+                        // with some tweaks.
+                        val impl = SVGDOMImplementation.getDOMImplementation()
 
-                val scrambler = PuzzlePlugins.WCA_PUZZLES[name]?.scrambler
-                    ?: return@get call.respondText("Invalid scrambler: $name")
-
-                val queryStr = call.request.uri.substringAfter('?', "")
-                val query = parseQuery(queryStr).toMutableMap()
-
-                val colorScheme = query["scheme"]?.let { scrambler.parseColorScheme(it) } ?: hashMapOf()
-                val scramble = query["scramble"]
-
-                when (extension) {
-                    "png" -> {
-                        if (query.containsKey("icon")) {
-                            val icon = PuzzleIcon.loadPuzzleIconPng(scrambler.shortName)
-
-                            call.respondBytes(icon.toByteArray(), ContentType.Image.PNG)
-                        } else {
-                            val svg = scrambler.drawScramble(scramble, colorScheme)
-                            val svgFile = svg.toString().byteInputStream()
-
-                            val (width, height) = scrambler.preferredSize.let { it.width to it.height }
-                            val imageTranscoder = BufferedImageTranscoder()
-
-                            // Copied from http://stackoverflow.com/a/6634963
-                            // with some tweaks.
-                            val impl = SVGDOMImplementation.getDOMImplementation()
-
-                            val hints = TranscodingHints().apply {
-                                this[ImageTranscoder.KEY_WIDTH] = width.toFloat()
-                                this[ImageTranscoder.KEY_HEIGHT] = height.toFloat()
-                                this[ImageTranscoder.KEY_DOM_IMPLEMENTATION] = impl
-                                this[ImageTranscoder.KEY_DOCUMENT_ELEMENT_NAMESPACE_URI] = SVGConstants.SVG_NAMESPACE_URI
-                                this[ImageTranscoder.KEY_DOCUMENT_ELEMENT_NAMESPACE_URI] = SVGConstants.SVG_NAMESPACE_URI
-                                this[ImageTranscoder.KEY_DOCUMENT_ELEMENT] = SVGConstants.SVG_SVG_TAG
-                                this[ImageTranscoder.KEY_XML_PARSER_VALIDATING] = false
-                            }
-
-                            imageTranscoder.transcodingHints = hints
-
-                            val input = TranscoderInput(svgFile)
-                            imageTranscoder.transcode(input, TranscoderOutput())
-
-                            val img = imageTranscoder.bufferedImage
-
-                            val bytes = ByteArrayOutputStream().also { ImageIO.write(img, "png", it) }
-
-                            call.respondBytes(bytes.toByteArray(), ContentType.Image.PNG)
+                        val hints = TranscodingHints().apply {
+                            this[ImageTranscoder.KEY_WIDTH] = width.toFloat()
+                            this[ImageTranscoder.KEY_HEIGHT] = height.toFloat()
+                            this[ImageTranscoder.KEY_DOM_IMPLEMENTATION] = impl
+                            this[ImageTranscoder.KEY_DOCUMENT_ELEMENT_NAMESPACE_URI] = SVGConstants.SVG_NAMESPACE_URI
+                            this[ImageTranscoder.KEY_DOCUMENT_ELEMENT_NAMESPACE_URI] = SVGConstants.SVG_NAMESPACE_URI
+                            this[ImageTranscoder.KEY_DOCUMENT_ELEMENT] = SVGConstants.SVG_SVG_TAG
+                            this[ImageTranscoder.KEY_XML_PARSER_VALIDATING] = false
                         }
-                    }
-                    "svg" -> {
-                        val svg = scrambler.drawScramble(scramble, colorScheme)
 
-                        call.respondText(svg.toString(), ContentType.Image.SVG)
-                    }
-                    "json" -> {
-                        val obj = PuzzleImageInfo(scrambler)
+                        imageTranscoder.transcodingHints = hints
 
-                        val dim = DimensionJsonData(obj.size.width, obj.size.height)
-                        val jsonData = PuzzleImageJsonData(dim, obj.colorScheme)
+                        val input = TranscoderInput(svgFile)
+                        imageTranscoder.transcode(input, TranscoderOutput())
 
-                        call.respond(jsonData)
+                        val img = imageTranscoder.bufferedImage
+
+                        val bytes = ByteArrayOutputStream().also {
+                            withContext(Dispatchers.IO) { ImageIO.write(img, "png", it) }
+                        }
+
+                        respondBytes(bytes.toByteArray(), ContentType.Image.PNG)
                     }
-                    else -> call.respondText("Invalid extension: $extension")
+                }
+                get("svg") {
+                    call.withScramble { _, svg -> respondText(svg.toString(), ContentType.Image.SVG) }
+                }
+                get("json") {
+                    call.withScramble { puzzle, _ ->
+                        val pzlSize = puzzle.preferredSize
+
+                        val dim = DimensionJsonData(pzlSize.width, pzlSize.height)
+                        val jsonData = PuzzleImageJsonData(dim, puzzle.defaultColorScheme)
+
+                        respond(jsonData)
+                    }
                 }
             }
+            route("{competitionName}") {
+                post("pdf") {
+                    call.withSheetsWCIF { wcif, _ ->
+                        val generationDate = LocalDate.now()
 
-            post("/{puzzleExt}") {
-                val puzzleExt = call.parameters["puzzleExt"] ?: return@post call.respondText("Please specify a puzzle")
+                        val totalPdfOutput = WCIFDataBuilder.wcifToCompletePdf(wcif, generationDate, environmentConfig.projectTitle)
 
-                val (name, extension) = splitNameAndExtension(puzzleExt)
-
-                if (extension.isEmpty()) {
-                    return@post call.respondText("No extension specified.")
-                }
-
-                val body = call.receiveText()
-                val query = parseQuery(body)
-
-                val reqRaw = query.getValue("sheets")
-                val scrambleRequests = JsonConfig.SERIALIZER.parse(ScrambleRequest.serializer().list, reqRaw)
-                val password = query["password"]
-
-                val wcif = WCIFScrambleMatcher.requestsToPseudoWCIF(scrambleRequests, name)
-
-                val generationDate = LocalDateTime.now()
-
-                when (extension) {
-                    "pdf" -> {
-                        val totalPdfOutput = WCIFDataBuilder.wcifToCompletePdf(wcif, generationDate.toLocalDate(), environmentConfig.projectTitle)
-
-                        call.response.header("Content-Disposition", "inline")
+                        response.header("Content-Disposition", "inline")
 
                         // Workaround for Chrome bug with saving PDFs:
                         // https://bugs.chromium.org/p/chromium/issues/detail?id=69677#c35
-                        call.response.header("Cache-Control", "public")
+                        response.header("Cache-Control", "public")
 
-                        call.respondBytes(totalPdfOutput.render(), ContentType.Application.Pdf)
+                        respondBytes(totalPdfOutput.render(), ContentType.Application.Pdf)
                     }
-                    "zip" -> {
-                        val generationUrl = query["generationUrl"]
+                }
+                post("zip") {
+                    call.withSheetsWCIF { wcif, query ->
+                        val generationUrl = query["generationUrl"].orEmpty()
                         val schedule = query["schedule"]
+                        val password = query["password"]
+
+                        val generationDate = LocalDateTime.now()
 
                         val parsedSchedule = schedule?.let { WCIFParser.parsePartial(it) }?.schedule ?: Schedule.EMPTY
                         val orderedWcif = wcif.copy(schedule = parsedSchedule)
@@ -172,15 +172,14 @@ class ScrambleViewHandler(val environmentConfig: ServerEnvironmentConfig) : Rout
                         val matchedWcif = WCIFScrambleMatcher.matchActivities(orderedWcif)
 
                         //val zipFile = WCIFBuilder.wcifToZip(name, generationDate, environmentConfig.projectTitle, scrambleRequests, password, generationUrl, wcif)
-                        val zipFile = WCIFDataBuilder.wcifToZip(matchedWcif, password, generationDate, environmentConfig.projectTitle, generationUrl.orEmpty())
+                        val zipFile = WCIFDataBuilder.wcifToZip(matchedWcif, password, generationDate, environmentConfig.projectTitle, generationUrl)
                         val zipOutput = zipFile.compress(password)
 
-                        val safeTitle = name.replace("\"".toRegex(), "'")
+                        val safeTitle = matchedWcif.shortName.replace("\"".toRegex(), "'")
 
-                        call.response.header("Content-Disposition", "attachment; filename=\"$safeTitle.zip\"")
-                        call.respondBytes(zipOutput, ContentType.Application.Zip)
+                        response.header("Content-Disposition", "attachment; filename=\"$safeTitle.zip\"")
+                        respondBytes(zipOutput, ContentType.Application.Zip)
                     }
-                    else -> call.respondText("Invalid extension: $extension")
                 }
             }
         }
