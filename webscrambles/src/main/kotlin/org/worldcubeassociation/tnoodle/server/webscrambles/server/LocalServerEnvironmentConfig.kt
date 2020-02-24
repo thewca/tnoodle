f62aeea195d8838662cce93ee4815019f8df2819
Gregor Billing <suushiemaniac@gmail.com>
2020-02-24 20:13:35 +0100

    Remove unused imports that caused :webscrambles assemble to fail

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/server/LocalServerEnvironmentConfig.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/server/LocalServerEnvironmentConfig.kt
@@ -3,17 +3,13 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.server
 import org.worldcubeassociation.tnoodle.server.util.ServerEnvironmentConfig
 import org.worldcubeassociation.tnoodle.server.util.WebServerUtils
 import org.worldcubeassociation.tnoodle.server.util.WebServerUtils.DEVEL_VERSION
-import org.worldcubeassociation.tnoodle.server.util.WebServerUtils.PRUNING_FOLDER
-import org.worldcubeassociation.tnoodle.server.util.WebServerUtils.jarFile
-import org.worldcubeassociation.tnoodle.server.util.WebServerUtils.programDirectory
-import java.io.*
 
 object LocalServerEnvironmentConfig : ServerEnvironmentConfig {
     override val projectName
-        get() = LocalServerEnvironmentConfig::class.java.getPackage()?.implementationTitle
+        get() = this::class.java.getPackage()?.implementationTitle
             ?: WebServerUtils.callerClass?.simpleName!!
 
     override val version
-        get() = LocalServerEnvironmentConfig::class.java.getPackage()?.implementationVersion
+        get() = this::class.java.getPackage()?.implementationVersion
             ?: DEVEL_VERSION
 }
