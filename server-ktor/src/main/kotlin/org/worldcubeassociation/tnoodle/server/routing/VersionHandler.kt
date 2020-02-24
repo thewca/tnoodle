ae2d0fdce877bc3101882e6411a3624968c7e85c
Gregor Billing <suushiemaniac@gmail.com>
2020-02-24 16:57:21 +0100

    Send public key bytes for additional verification

--- server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/routing/VersionHandler.kt
+++ server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/routing/VersionHandler.kt
@@ -9,12 +9,19 @@ import kotlinx.serialization.json.json
 import org.worldcubeassociation.tnoodle.server.RouteHandler
 import org.worldcubeassociation.tnoodle.server.signature.BuildVerification
 
+import java.util.*
+
 class VersionHandler(val versionKey: String) : RouteHandler {
     override fun install(router: Routing) {
         router.get("/version") {
+            val keyBytes = BuildVerification.PUBLIC_KEY_BYTES?.let {
+                Base64.getEncoder().encode(it)
+            }
+
             val json = json {
                 "runningVersion" to versionKey
                 "officialBuild" to BuildVerification.BUILD_VERIFIED
+                "keyBytes" to keyBytes
             }
 
             call.respond(json)
