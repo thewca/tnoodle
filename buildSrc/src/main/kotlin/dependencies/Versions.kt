d2fa68f928d11c287d17f08a8aeddb3dc22889e1
Gregor Billing <suushiemaniac@gmail.com>
2020-02-24 17:18:25 +0100

    Bump dependency versions

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -1,18 +1,18 @@
 package dependencies
 
 object Versions {
-    val JUNIT_JUPITER = "5.4.2"
+    val JUNIT_JUPITER = "5.6.0"
     val BATIK = "1.12"
     val KOTLIN = "1.3.61"
-    val KTOR = "1.3.0"
+    val KTOR = "1.3.1"
     val PROGUARD = "6.2.2"
     val KOTLESS = "0.1.3"
 
     val MARKDOWNJ_CORE = "0.4"
-    val ZIP4J = "2.3.1"
+    val ZIP4J = "2.3.2"
     val ITEXTPDF = "5.5.13.1"
     val BATIK_TRANSCODER = BATIK
-    val SNAKEYAML = "1.25"
+    val SNAKEYAML = "1.26"
     val SYSTEM_TRAY = "3.17"
     val BOUNCYCASTLE = "1.64"
     val JUNIT_JUPITER_API = JUNIT_JUPITER
@@ -30,15 +30,15 @@ object Versions {
     val PROGUARD_GRADLE = PROGUARD
     val WCA_I18N = "0.4.3"
     val GOOGLE_APPENGINE_GRADLE = "2.2.0"
-    val GOOGLE_CLOUD_STORAGE = "1.103.0"
+    val GOOGLE_CLOUD_STORAGE = "1.104.0"
     val TNOODLE_SCRAMBLES = "0.17.0"
     val APACHE_COMMONS_LANG3 = "3.9"
     val KOTLESS_KTOR = KOTLESS
 
     object Plugins {
         val SHADOW = "5.2.0"
-        val NODEJS = "2.2.1"
-        val DEPENDENCY_VERSIONS = "0.27.0"
+        val NODEJS = "2.2.2"
+        val DEPENDENCY_VERSIONS = "0.28.0"
         val GIT_VERSION_TAG = "0.12.2"
 
         val KOTLIN = Versions.KOTLIN
