4a18134b763ccfda8ff08f927f09a77535046309
Alexandre Campos <camposalexandreh@gmail.com>
2020-02-26 19:35:14 -0300

    Make it a really Single Page

--- tnoodle-ui/src/App.js
+++ tnoodle-ui/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
 import Index from "./components/Index";
-import OfflineScrambler from "./components/OfflineScrambler";
-import OnlineScrambler from "./components/OnlineScrambler";
-import TnoodleNavbar from "./components/TnoodleNavbar";
-import ManageCompetition from "./components/ManageCompetition";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -18,39 +13,9 @@ class App extends Component {
 
     render() {
         return (
-            <HashRouter basename={this.homeLink}>
-                <div className="App">
-                    <TnoodleNavbar
-                        homeLink={this.homeLink}
-                        offlineScramblerLink={this.offlineScramblerLink}
-                        onlineScramblerLink={this.onlineScramblerLink}
-                    />
-                    <Switch>
-                        <Route path={this.offlineScramblerLink}>
-                            <OfflineScrambler />
-                        </Route>
-                        <Route path={this.onlineScramblerLink}>
-                            <OnlineScrambler />
-                        </Route>
-                        <Route
-                            path={this.competitionLink}
-                            component={props => (
-                                <ManageCompetition
-                                    competitionId={
-                                        props.match.params.competitionId
-                                    }
-                                />
-                            )}
-                        ></Route>
-                        <Route path={this.homeLink}>
-                            <Index
-                                offlineScramblerLink={this.offlineScramblerLink}
-                                onlineScramblerLink={this.onlineScramblerLink}
-                            />
-                        </Route>
-                    </Switch>
-                </div>
-            </HashRouter>
+            <div className="App">
+                <Index />
+            </div>
         );
     }
 }
