8e1d04782d97d164892008c605ef8a75107c016b
Gregor Billing <suushiemaniac@gmail.com>
2020-02-28 13:49:57 +0100

    Add private RSA key support

--- server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/signature/BuildVerification.kt
+++ server-ktor/src/main/kotlin/org/worldcubeassociation/tnoodle/server/signature/BuildVerification.kt
@@ -1,10 +1,8 @@
 package org.worldcubeassociation.tnoodle.server.signature
 
 import org.bouncycastle.util.io.pem.PemReader
-import java.security.KeyFactory
-import java.security.PublicKey
-import java.security.Signature
-import java.security.SignatureException
+import java.security.*
+import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 import java.util.*
 
@@ -13,16 +11,22 @@ object BuildVerification {
     const val SIGNATURE_ALGORITHM = "SHA256with$ENCRYPTION_ALGORITHM"
 
     private const val PUBLIC_KEY_PEM = "/signature/tnoodle_public.pem"
+    private const val PRIVATE_KEY_PEM8 = "/signature/tnoodle_private.pkcs8.pem"
 
     val BUILD_VERIFIED by lazy { checkBuildSignature(PUBLIC_KEY_PEM) }
 
-    val PUBLIC_KEY_BYTES = loadPublicKeyBytes(PUBLIC_KEY_PEM)
+    val PUBLIC_KEY_BYTES = loadRSAKeyBytes(PUBLIC_KEY_PEM)
     val PUBLIC_KEY_BYTES_BASE64 = PUBLIC_KEY_BYTES?.let { Base64.getEncoder().encodeToString(it) }
+
+    val PRIVATE_KEY_BYTES = loadRSAKeyBytes(PRIVATE_KEY_PEM8)
+
+    val RSA_PUBLIC_KEY by lazy { PUBLIC_KEY_BYTES?.let(this::loadPublicKey) }
+    val RSA_PRIVATE_KEY by lazy { PRIVATE_KEY_BYTES?.let(this::loadPrivateKey) }
 
     private val keyFactory = KeyFactory.getInstance(ENCRYPTION_ALGORITHM)
     private val signatureInstance = Signature.getInstance(SIGNATURE_ALGORITHM)
 
-    fun loadPublicKeyBytes(resourcePath: String): ByteArray? {
+    fun loadRSAKeyBytes(resourcePath: String): ByteArray? {
         return this::class.java.getResourceAsStream(resourcePath)?.reader()
             ?.use { PemReader(it).readPemObject().content }
     }
@@ -32,8 +36,13 @@ object BuildVerification {
         return keyFactory.generatePublic(spec)
     }
 
+    fun loadPrivateKey(keyBytes: ByteArray): PrivateKey {
+        val spec = PKCS8EncodedKeySpec(keyBytes)
+        return keyFactory.generatePrivate(spec)
+    }
+
     fun checkBuildSignature(resourcePath: String): Boolean {
-        val publicKey = PUBLIC_KEY_BYTES?.let(this::loadPublicKey) ?: return false
+        val publicKey = RSA_PUBLIC_KEY ?: return false
 
         val fileBytes = this::class.java.getResourceAsStream(resourcePath)?.readBytes()
             ?: return false
