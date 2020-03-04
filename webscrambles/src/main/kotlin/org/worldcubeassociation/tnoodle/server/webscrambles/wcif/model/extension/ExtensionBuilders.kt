cdf38bc86b3625e1a833431606342ab57a1ccdbc
Gregor Billing <suushiemaniac@gmail.com>
2020-03-04 19:12:45 +0100

    Fix sealed class serialDecriptor reading

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/extension/ExtensionBuilders.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/extension/ExtensionBuilders.kt
@@ -21,8 +21,11 @@ sealed class ExtensionBuilder {
     }
 
     companion object {
-        val REGISTERED_CHILDREN = List(serializer().descriptor.elementsCount) {
-            serializer().descriptor.getElementName(it)
+        val NESTED_DESCRIPTOR_INDEX = serializer().descriptor.getElementIndex("value")
+        val NESTED_DESCRIPTOR = serializer().descriptor.getElementDescriptor(NESTED_DESCRIPTOR_INDEX)
+
+        val REGISTERED_CHILDREN = List(NESTED_DESCRIPTOR.elementsCount) {
+            NESTED_DESCRIPTOR.getElementName(it)
         }
     }
 }
