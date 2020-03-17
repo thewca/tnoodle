6f5aee7048d5363247a145a971e04f31753f7325
Gregor Billing <suushiemaniac@gmail.com>
2020-03-17 12:22:47 +0100

    Add formatId Provider

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Round.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Round.kt
@@ -3,15 +3,17 @@ package org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model
 import kotlinx.serialization.Serializable
 import org.worldcubeassociation.tnoodle.server.plugins.FormatPlugins
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.extension.ExtensionProvider
+import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.provider.FormatIdProvider
 
 @Serializable
-data class Round(val id: String, val format: String, val scrambleSetCount: Int, val scrambleSets: List<ScrambleSet> = emptyList(), override val extensions: List<Extension> = emptyList()): ExtensionProvider() {
+data class Round(val id: String, val format: String, val scrambleSetCount: Int, val scrambleSets: List<ScrambleSet> = emptyList(), override val extensions: List<Extension> = emptyList()): ExtensionProvider(), FormatIdProvider {
     val idCode: ActivityCode
         get() = ActivityCode(id)
 
-    val formatPlugin: FormatPlugins?
-        get() = FormatPlugins.WCA_FORMATS[format]
+    override val formatId: String
+        get() = format
 
     val expectedAttemptNum: Int
-        get() = formatPlugin?.numSolves ?: 5
+        get() = formatPlugin?.numSolves
+            ?: FormatPlugins.DEFAULT_FALLBACK_FORMAT.numSolves
 }
