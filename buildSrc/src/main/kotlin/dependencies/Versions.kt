63176b24506655bdea4db49a9e45baa750237798
Gregor Billing <suushiemaniac@gmail.com>
2020-03-31 13:42:24 +0200

    Bump versions for 1.0 RC

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -1,15 +1,15 @@
 package dependencies
 
 object Versions {
-    val JUNIT_JUPITER = "5.6.0"
+    val JUNIT_JUPITER = "5.6.1"
     val BATIK = "1.12"
-    val KOTLIN = "1.3.70"
+    val KOTLIN = "1.3.71"
     val KTOR = "1.3.2"
     val PROGUARD = "6.2.2"
     val KOTLESS = "0.1.3"
 
     val MARKDOWNJ_CORE = "0.4"
-    val ZIP4J = "2.3.2"
+    val ZIP4J = "2.5.1"
     val ITEXTPDF = "5.5.13.1"
     val BATIK_TRANSCODER = BATIK
     val SNAKEYAML = "1.26"
@@ -21,7 +21,7 @@ object Versions {
     val KOTLIN_STDLIB_JS = KOTLIN
     val KOTLIN_STDLIB_COMMON = KOTLIN
     val KOTLIN_SERIALIZATION_JVM = "0.20.0"
-    val KOTLIN_COROUTINES_CORE = "1.3.4"
+    val KOTLIN_COROUTINES_CORE = "1.3.5"
     val KTOR_SERVER_NETTY = KTOR
     val KTOR_SERVER_SERVLET = KTOR
     val KTOR_SERIALIZATION = KTOR
@@ -31,16 +31,16 @@ object Versions {
     val PROGUARD_GRADLE = PROGUARD
     val WCA_I18N = "0.4.3"
     val GOOGLE_APPENGINE_GRADLE = "2.2.0"
-    val GOOGLE_CLOUD_STORAGE = "1.103.0"
-    val TNOODLE_SCRAMBLES = "0.17.0"
-    val APACHE_COMMONS_LANG3 = "3.9"
+    val GOOGLE_CLOUD_STORAGE = "1.106.0"
+    val TNOODLE_SCRAMBLES = "0.18.0"
+    val APACHE_COMMONS_LANG3 = "3.10"
     val KOTLESS_KTOR = KOTLESS
 
     object Plugins {
         val SHADOW = "5.2.0"
-        val NODEJS = "2.2.2"
+        val NODEJS = "2.2.3"
         val DEPENDENCY_VERSIONS = "0.28.0"
-        val GIT_VERSION_TAG = "0.12.2"
+        val GIT_VERSION_TAG = "0.12.3"
 
         val KOTLIN = Versions.KOTLIN
 
