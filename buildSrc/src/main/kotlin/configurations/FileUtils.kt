a2b7630ba4b4f891e7f124a0862dafcef556adb2
Gregor Billing <suushiemaniac@gmail.com>
2020-05-21 16:22:48 +0200

    Be more cautious about creating symlinks in generic file systems

--- buildSrc/src/main/kotlin/configurations/FileUtils.kt
+++ buildSrc/src/main/kotlin/configurations/FileUtils.kt
@@ -1,11 +1,26 @@
 package configurations
 
-import org.gradle.api.Project
+import org.gradle.api.Task
 import java.io.File
+import java.io.IOException
 import java.nio.file.Files
 
 object FileUtils {
-    fun Project.symlink(linkName: File, originFile: File) {
-        Files.createSymbolicLink(linkName.toPath(), originFile.toPath())
+    fun Task.symlink(linkName: File, originFile: File): Boolean {
+        return try {
+            if (linkName.exists() && !linkName.delete()) {
+                logger.warn("Unable to create symlink: Cannot override previously existing file with same name as target")
+                return false
+            }
+
+            Files.createSymbolicLink(linkName.toPath(), originFile.toPath())
+            true
+        } catch (e: IOException) {
+            logger.warn("Unable to create symlink: IOException", e)
+            false
+        } catch (e: SecurityException) {
+            logger.warn("Unable to create symlink: SecurityException", e)
+            false
+        }
     }
 }
