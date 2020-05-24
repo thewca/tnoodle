ec04cb32812368af13b743709ed87b67c7ebf688
Gregor Billing <suushiemaniac@gmail.com>
2020-05-24 21:29:58 +0200

    Add back CLI flag for manual port control

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/WebscramblesServer.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/WebscramblesServer.kt
@@ -1,6 +1,7 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles
 
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -89,18 +90,22 @@ class WebscramblesServer(val environmentConfig: ServerEnvironmentConfig) : Appli
 
             val desiredJsEnv by parser.adding("--jsenv", help = "Add entry to global js object TNOODLE_ENV in /env.js. Treated as strings, so FOO=42 will create the entry TNOODLE_ENV['FOO'] = '42';")
 
+            val port by parser.storing("-p", "--port", help = "Start TNoodle on given port. Should be numeric. Defaults to 2014", transform = { toInt() }).default(OfflineJarUtils.TNOODLE_PORT)
+
             val noBrowser by parser.flagging("-n", "--nobrowser", help = "Don't open the browser when starting the server")
             val noUpgrade by parser.flagging("-u", "--noupgrade", help = "If an instance of TNoodle is running on the desired port(s), do not attempt to kill it and start up")
             val noReexec by parser.flagging(NO_REEXEC_OPT, help = "Do not reexec. This is sometimes done to rename java.exe on Windows, or to get a larger heap size.")
 
-            OfflineJarUtils.setApplicationIcon()
+            val offlineHandler = OfflineJarUtils(port)
+
+            offlineHandler.setApplicationIcon()
 
             if (!noReexec) {
                 MainLauncher.wrapMain(args, MIN_HEAP_SIZE_MEGS)
 
                 // This second call to setApplicationIcon() is intentional.
                 // We want different icons for the parent and child processes.
-                OfflineJarUtils.setApplicationIcon()
+                offlineHandler.setApplicationIcon()
             }
 
             for (jsEnv in desiredJsEnv) {
@@ -110,20 +115,23 @@ class WebscramblesServer(val environmentConfig: ServerEnvironmentConfig) : Appli
 
             if (!noUpgrade) {
                 try {
-                    val shutdownMaybe = URL("http://localhost:2014${TNoodleServer.KILL_URL}").openStream()
+                    val shutdownMaybe = URL("http://localhost:$port${TNoodleServer.KILL_URL}").openStream()
                     shutdownMaybe.close()
                 } catch (ignored: IOException) {
-                    // NOOP. This means we couldn't connect to localhost:2014
+                    // NOOP. This means we couldn't connect to localhost:$PORT
                     // in which case the shutdown attempt is unnecessary anyways.
                 }
             }
 
-            val cliEnv = commandLineEnvironment(args)
+            // ktor specifically requires ONE dash, but our parsing library specifically requires TWO dashes
+            val ktorArgs = args + arrayOf("-port=$port")
+
+            val cliEnv = commandLineEnvironment(ktorArgs)
             embeddedServer(Netty, cliEnv).start()
 
             LOG.info("${LocalServerEnvironmentConfig.projectTitle} started")
 
-            val url = OfflineJarUtils.openTabInBrowser(!noBrowser)
+            val url = offlineHandler.openTabInBrowser(!noBrowser)
             LOG.info("Visit $url for a readme and demo.")
         }
     }
