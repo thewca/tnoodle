c37736a2eb6059cd70c3a697de9cacee32a57a8d
Gregor Billing <suushiemaniac@gmail.com>
2020-05-25 11:26:22 +0200

    Strongly type frontend data classes

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/frontend/ApplicationDataHandler.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/routing/frontend/ApplicationDataHandler.kt
@@ -5,38 +5,23 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.json
 import org.worldcubeassociation.tnoodle.server.RouteHandler
 import org.worldcubeassociation.tnoodle.server.model.EventData
 import org.worldcubeassociation.tnoodle.server.model.FormatData
+import org.worldcubeassociation.tnoodle.server.webscrambles.serial.EventFrontendData
+import org.worldcubeassociation.tnoodle.server.webscrambles.serial.FormatFrontendData
 
 object ApplicationDataHandler : RouteHandler {
     override fun install(router: Route) {
         router.route("data") {
             get("events") {
-                val eventData = EventData.values().map {
-                    json {
-                        "id" to it.key
-                        "name" to it.description
-                        "format_ids" to JsonArray(it.legalFormats.map(FormatData::key).map(::JsonPrimitive))
-                        "can_change_time_limit" to (it !in EventData.ONE_HOUR_EVENTS)
-                        "is_timed_event" to (it !in EventData.ONE_HOUR_EVENTS)
-                        "is_fewest_moves" to (it == EventData.THREE_FM)
-                        "is_multiple_blindfolded" to (it == EventData.THREE_MULTI_BLD)
-                    }
-                }
-
+                val eventData = EventData.values().map(EventFrontendData.Companion::fromDataModel)
                 call.respond(eventData)
             }
 
             get("formats") {
                 val formatData = FormatData.WCA_FORMATS.mapValues {
-                    json {
-                        "name" to it.value.description
-                        "shortName" to it.value.tag
-                    }
+                    FormatFrontendData.fromDataModel(it.value)
                 }
 
                 call.respond(formatData)
