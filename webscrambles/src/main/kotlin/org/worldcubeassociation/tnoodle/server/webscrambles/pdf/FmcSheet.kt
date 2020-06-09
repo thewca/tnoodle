7d7d632ac509b04e5f880b7072c41f5f235c6fc4
Gregor Billing <suushiemaniac@gmail.com>
2020-06-09 19:30:58 +0200

    Fix localization quirks across FMC sheet implementations

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/FmcSheet.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/pdf/FmcSheet.kt
@@ -4,7 +4,7 @@ import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.*
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model.extension.FmcAttemptCountExtension
 import java.util.Locale
 
-abstract class FmcSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode, val competitionTitle: String, val locale: Locale) : BaseScrambleSheet(scrambleSet, activityCode) {
+abstract class FmcSheet(scrambleSet: ScrambleSet, activityCode: ActivityCode, val competitionTitle: String, val locale: Locale, val hasGroupID: Boolean) : BaseScrambleSheet(scrambleSet, activityCode) {
     val expectedAttemptNum: Int
         get() = scrambleSet.findExtension<FmcAttemptCountExtension>()
             ?.totalAttempts ?: 1
