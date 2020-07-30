3f518123c884fd18c02c99b91d11c1ff33868a76
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-07-30 20:42:22 -0300

    Add 1 round of 333 as default

--- tnoodle-ui/src/constants/default.wcif.js
+++ tnoodle-ui/src/constants/default.wcif.js
@@ -1,9 +1,24 @@
+import { getDefaultCopiesExtension } from "../api/tnoodle.api";
+
+// Add 1 round of 3x3x3
+let default333 = {
+    id: "333",
+    rounds: [
+        {
+            format: "a",
+            id: "333-r1",
+            scrambleSetCount: 1,
+            extensions: [getDefaultCopiesExtension()],
+        },
+    ],
+};
+
 export const defaultWcif = {
     formatVersion: "1.0",
     name: "",
     shortName: "",
     id: "",
-    events: [],
+    events: [default333],
     persons: [],
     schedule: { numberOfDays: 0, venues: [] },
 };
