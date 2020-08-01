93b39bedf24f9d6f5c630aaea275dd02bc205b4e
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-08-01 13:03:03 -0300

    Show intercepted message

--- tnoodle-ui/src/App.js
+++ tnoodle-ui/src/App.js
@@ -2,21 +2,62 @@ import React, { Component } from "react";
 import EntryInterface from "./components/EntryInterface";
 import EventPickerTable from "./components/EventPickerTable";
 import SideBar from "./components/SideBar";
-import FlashMessage from "./components/FlashMessage";
 import VersionInfo from "./components/VersionInfo";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import fetchIntercept from "fetch-intercept";
 
-// http-interceptor
-fetchIntercept.register({
-    responseError: function (error) {
-        debugger;
-        return Promise.reject(error);
-    },
-});
-
 class App extends Component {
+    constructor() {
+        super();
+
+        const that = this;
+
+        // http interceptor
+        fetchIntercept.register({
+            response: function (response) {
+                if (!response.ok) {
+                    that.handleHttpError(response);
+                }
+                return response;
+            },
+
+            responseError: function (error) {
+                that.handleHttpError(error);
+                return Promise.reject(error);
+            },
+        });
+
+        this.state = { errorMessage: "" };
+    }
+
+    handleHttpError = (error) => {
+        let message = error.statusText || JSON.stringify(error);
+        this.updateErrorMessage(message);
+    };
+
+    updateErrorMessage = (errorMessage) => {
+        // Clear the message after some seconds
+        setTimeout(() => {
+            this.setState({ ...this.state, errorMessage: "" });
+        }, 1000 * 10);
+
+        this.setState({ ...this.state, errorMessage });
+    };
+
+    maybeShowErrorMessage = () => {
+        if (!this.state.errorMessage) {
+            return null;
+        }
+        return (
+            <div className="row sticky-top">
+                <div className={"col-12 alert alert-danger"}>
+                    {this.state.errorMessage}
+                </div>
+            </div>
+        );
+    };
+
     render() {
         return (
             <div className="App">
@@ -29,7 +70,7 @@ class App extends Component {
                             <SideBar />
                         </div>
                         <div className="col-9">
-                            <FlashMessage />
+                            {this.maybeShowErrorMessage()}
                             <VersionInfo />
                             <EntryInterface />
                             <EventPickerTable />
