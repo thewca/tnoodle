4e1ed1316aace560a97fb5adef5afe3c74454ca8
Gregor Billing <suushiemaniac@gmail.com>
2020-09-08 15:07:19 +0200

    Add script to generate Java-compatible RSA Key Pair

---
+++ genrsa.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+openssl genrsa -out tnoodle_private.pem 4096
+openssl rsa -pubout -in tnoodle_private.pem -out tnoodle_public.pem
+openssl pkcs8 -topk8 -in tnoodle_private.pem -inform pem -out tnoodle_private.pkcs8.pem -outform pem -nocrypt
+
+mv tnoodle_public.pem tnoodle-server/src/main/resources/rsa/
+rm tnoodle_private.pem
+
+grep -v "^wca.wst.signature-private-key" gradle.properties > gradle.properties.tmp
+echo "wca.wst.signature-private-key=tnoodle_private.pkcs8.pem" >> gradle.properties.tmp
+mv gradle.properties.tmp gradle.properties
