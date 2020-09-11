f7a43f527a200166733cf9fa0cc8456622aaec3f
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-09-10 23:33:14 -0300

    Add test for SideBar

---
+++ tnoodle-ui/src/components/SideBar.test.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { act } from "react-dom/test-utils";
+
+import { render, unmountComponentAtNode } from "react-dom";
+
+import { Provider } from "react-redux";
+import store from "../redux/Store";
+
+import SideBar from "./SideBar";
+
+const wcaApi = require("../api/wca.api");
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("Each competition fetched from the website must become a button", async () => {
+    // Define mock objects
+    const year = new Date().getFullYear();
+    const competitions = [
+        {
+            id: "WCAWorldChampionship" + year,
+            name: "WCA World Championship " + year,
+        },
+        {
+            id: "Nationals" + year,
+            name: "Nationals " + year,
+        },
+    ];
+
+    // Turn on mocking behavior
+    jest.spyOn(wcaApi, "isLogged").mockImplementation(() => true);
+
+    jest.spyOn(
+        wcaApi,
+        "getUpcomingManageableCompetitions"
+    ).mockImplementation(() => Promise.resolve(competitions));
+
+    // Render component
+    await act(async () => {
+        render(
+            <Provider store={store}>
+                <SideBar />
+            </Provider>,
+            container
+        );
+    });
+
+    const buttons = Array.from(container.querySelectorAll("button"));
+
+    // First button should be manual selection
+    expect(buttons[0].innerHTML).toBe("Manual Selection");
+
+    // Last button should be Log Out
+    expect(buttons[buttons.length - 1].innerHTML).toBe("Log Out");
+
+    // Each competition must have a button
+    for (let i = 0; i < competitions.length; i++) {
+        expect(competitions[i].name).toBe(buttons[i + 1].innerHTML);
+    }
+
+    // Clear mock
+    wcaApi.isLogged.mockRestore();
+    wcaApi.getUpcomingManageableCompetitions.mockRestore();
+});
