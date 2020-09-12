b22f5d0f18e4aac7e7981fcd0ed954a84ca9acf2
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-09-12 17:52:46 -0300

    Check if Select Suggested is submit as well

---
+++ tnoodle-ui/src/test/mock/wca.api.mock.js
@@ -0,0 +1,16 @@
+const year = new Date().getFullYear();
+export const competitions = [
+    {
+        id: "WCAWorldChampionship" + year,
+        name: "WCA World Championship " + year,
+    },
+    {
+        id: "Nationals" + year,
+        name: "Nationals " + year,
+    },
+];
+
+export const me = {
+    wca_id: "2010AAAA01",
+    name: "User Name",
+};
