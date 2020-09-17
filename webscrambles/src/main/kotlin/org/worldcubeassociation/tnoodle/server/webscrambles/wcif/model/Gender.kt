b13e6fc3258e0caaed30d8d972383833240dbd5c
Gregor Billing <suushiemaniac@gmail.com>
2020-09-18 00:35:41 +0200

    Do not rely on Java lowercasing mechanisms

--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Gender.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/Gender.kt
@@ -1,6 +1,7 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model
 
 import org.worldcubeassociation.tnoodle.server.serial.SingletonStringEncoder
+import org.worldcubeassociation.tnoodle.server.webscrambles.exceptions.BadWcifParameterException
 
 enum class Gender(val wcaString: String) {
     MALE("m"),
@@ -11,6 +12,7 @@ enum class Gender(val wcaString: String) {
         fun fromWCAString(wcaString: String) = values().find { it.wcaString == wcaString }
 
         override fun encodeInstance(instance: Gender) = instance.wcaString
-        override fun makeInstance(deserialized: String) = fromWCAString(deserialized)!!
+        override fun makeInstance(deserialized: String) = fromWCAString(deserialized)
+            ?: BadWcifParameterException.error("Unknown WCIF spec Gender: '$deserialized'. Valid types: ${values().map { it.wcaString }}")
     }
 }
--- webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/ResultType.kt
+++ webscrambles/src/main/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/wcif/model/ResultType.kt
@@ -1,18 +1,17 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles.wcif.model
 
 import org.worldcubeassociation.tnoodle.server.serial.SingletonStringEncoder
+import org.worldcubeassociation.tnoodle.server.webscrambles.exceptions.BadWcifParameterException
 
-enum class ResultType {
-    SINGLE,
-    AVERAGE;
-
-    val wcaString
-        get() = this.name.toLowerCase()
+enum class ResultType(val wcaString: String) {
+    SINGLE("single"),
+    AVERAGE("average");
 
     companion object : SingletonStringEncoder<ResultType>("ResultType") {
         fun fromWCAString(wcaString: String) = values().find { it.wcaString == wcaString }
 
         override fun encodeInstance(instance: ResultType) = instance.wcaString
-        override fun makeInstance(deserialized: String) = fromWCAString(deserialized)!!
+        override fun makeInstance(deserialized: String) = fromWCAString(deserialized)
+            ?: BadWcifParameterException.error("Unknown WCIF spec ResultType: '$deserialized'. Valid types: ${values().map { it.wcaString }}")
     }
 }
