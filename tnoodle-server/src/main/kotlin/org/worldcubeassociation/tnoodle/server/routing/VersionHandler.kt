54a30871b65fe3e5c5978c7db74d4338533cb6ed
Gregor Billing <suushiemaniac@gmail.com>
2020-09-29 14:02:13 +0200

    Rename log file

--- tnoodle-server/src/main/kotlin/org/worldcubeassociation/tnoodle/server/routing/VersionHandler.kt
+++ tnoodle-server/src/main/kotlin/org/worldcubeassociation/tnoodle/server/routing/VersionHandler.kt
@@ -4,10 +4,8 @@ import io.ktor.application.call
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
-import kotlinx.serialization.json.json
 
 import org.worldcubeassociation.tnoodle.server.RouteHandler
-import org.worldcubeassociation.tnoodle.server.crypto.AsymmetricCipher
 import org.worldcubeassociation.tnoodle.server.ServerEnvironmentConfig
 import org.worldcubeassociation.tnoodle.server.serial.VersionInfo
 
