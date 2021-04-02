9a87b66cab0ab4e2e522cbb53845e5e8553ecf74
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-02 17:26:04 -0300

    Migrate mbld detail

--- tnoodle-ui/src/main/components/MbldDetail.tsx
+++ tnoodle-ui/src/main/components/MbldDetail.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { MBLD_MIN } from "../constants/wca.constants";
-import { updateMbld, updateFileZipBlob } from "../redux/ActionCreators";
+import RootState from "../model/RootState";
+import { updateFileZipBlob, updateMbld } from "../redux/ActionCreators";
 
 const MbldDetail = () => {
-    const mbld = useSelector((state) => state.mbld);
-    const bestMbldAttempt = useSelector((state) => state.bestMbldAttempt);
+    const mbld = useSelector((state: RootState) => state.mbld);
+    const bestMbldAttempt = useSelector(
+        (state: RootState) => state.bestMbldAttempt
+    );
     const generatingScrambles = useSelector(
-        (state) => state.generatingScrambles
+        (state: RootState) => state.generatingScrambles
     );
 
     const dispatch = useDispatch();
 
-    const handleMbldChange = (evt) => {
-        dispatch(updateMbld(evt.target.value));
+    const handleMbldChange = (newMbld: string) => {
+        dispatch(updateMbld(newMbld));
         dispatch(updateFileZipBlob(null));
     };
 
@@ -28,14 +30,14 @@ const MbldDetail = () => {
                         className="form-control bg-dark text-white"
                         type="number"
                         value={mbld}
-                        onChange={handleMbldChange}
+                        onChange={(e) => handleMbldChange(e.target.value)}
                         min={MBLD_MIN}
                         required
                         disabled={generatingScrambles}
                     />
                 </td>
             </tr>
-            {mbld < bestMbldAttempt && (
+            {!!mbld && Number(mbld) < bestMbldAttempt && (
                 <tr className="alert alert-warning">
                     <th colSpan={4}>
                         {`You selected ${mbld} cubes for Multi-Blind, but there's a competitor who already tried ${bestMbldAttempt} at a competition. Proceed if you are really certain of it.`}
--- tnoodle-ui/src/main/model/RootState.ts
+++ tnoodle-ui/src/main/model/RootState.ts
@@ -6,11 +6,13 @@ import WcaEvent from "./WcaEvent";
 import Wcif from "./Wcif";
 
 export default interface RootState {
+    bestMbldAttempt: number;
     cachedObjects: Record<string, CachedObject>;
     competitionId: string;
     competitions: Competition[];
     editingDisabled: boolean;
     generatingScrambles: boolean;
+    mbld: string;
     me: Me;
     password: string;
     suggestedFmcTranslations: string[];
