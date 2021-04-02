391abb5aed27e70c55fd485cd7f6234939e8196c
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-02 18:53:23 -0300

    Migrate Main from class to function

--- tnoodle-ui/src/main/components/Main.jsx
+++ tnoodle-ui/src/main/components/Main.jsx
@@ -1,189 +1,155 @@
-import React, { Component } from "react";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchZip } from "../api/tnoodle.api";
+import { ScrambleClient } from "../api/tnoodle.socket";
+import { isUsingStaging } from "../api/wca.api";
+import {
+    resetScramblingProgressCurrent,
+    updateFileZipBlob,
+    updateGeneratingScrambles,
+    updateScramblingProgressCurrentEvent,
+    updateScramblingProgressTarget,
+} from "../redux/ActionCreators";
 import EntryInterface from "./EntryInterface";
 import EventPickerTable from "./EventPickerTable";
 import Interceptor from "./Interceptor";
-import VersionInfo from "./VersionInfo";
-import { fetchZip } from "../api/tnoodle.api";
-import { ScrambleClient } from "../api/tnoodle.socket";
-import {
-    updateFileZipBlob,
-    updateGeneratingScrambles,
-    updateScramblingProgressTarget,
-    updateScramblingProgressCurrentEvent,
-    resetScramblingProgressCurrent,
-} from "../redux/ActionCreators";
-import { connect } from "react-redux";
-import { isUsingStaging } from "../api/wca.api";
 import "./Main.css";
+import VersionInfo from "./VersionInfo";
 
-const mapStateToProps = (store) => ({
-    wcif: store.wcif,
-    mbld: store.mbld,
-    password: store.password,
-    competitionId: store.competitionId,
-    officialZip: store.officialZip,
-    fileZipBlob: store.fileZipBlob,
-    translations: store.translations,
-    generatingScrambles: store.generatingScrambles,
-});
+const Main = () => {
+    const [competitionNameFileZip, setCompetitionNameFileZip] = useState("");
+    const mbld = useSelector((state) => state.mbld);
+    const password = useSelector((state) => state.password);
+    const translations = useSelector((state) => state.translations);
+    const wcif = useSelector((state) => state.wcif);
+    const competitionId = useSelector((state) => state.competitionId);
+    const generatingScrambles = useSelector(
+        (state) => state.generatingScrambles
+    );
+    const officialZip = useSelector((state) => state.officialZip);
+    const fileZipBlob = useSelector((state) => state.fileZipBlob);
 
-const mapDispatchToProps = {
-    updateFileZipBlob,
-    updateGeneratingScrambles,
-    updateScramblingProgressTarget,
-    updateScramblingProgressCurrentEvent,
-    resetScramblingProgressCurrent,
-};
+    const interceptorRef = useRef();
 
-const Main = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(
-    class extends Component {
-        constructor(props) {
-            super(props);
+    const dispatch = useDispatch();
 
-            this.state = {
-                competitionNameFileZip: "",
-            };
+    const onSubmit = (evt) => {
+        evt.preventDefault();
 
-            this.interceptor = React.createRef();
+        if (generatingScrambles) {
+            return;
         }
-        onSubmit = (evt) => {
-            evt.preventDefault();
 
-            if (this.props.generatingScrambles) {
-                return;
-            }
+        if (!!fileZipBlob) {
+            downloadZip();
+        } else {
+            generateZip();
+        }
+    };
 
-            if (this.props.fileZipBlob != null) {
-                this.downloadZip();
-            } else {
-                this.generateZip();
-            }
-        };
+    const generateZip = () => {
+        setCompetitionNameFileZip(wcif.name);
 
-        generateZip = () => {
-            this.setState({
-                ...this.state,
-                competitionNameFileZip: this.props.wcif.name,
+        let scrambleClient = new ScrambleClient(
+            updateScramblingProgressTarget,
+            updateScramblingProgressCurrentEvent
+        );
+
+        fetchZip(scrambleClient, wcif, mbld, password, translations)
+            .then((blob) => dispatch(updateFileZipBlob(blob)))
+            .catch((err) => interceptorRef.current.updateMessage(err))
+            .finally(() => {
+                dispatch(updateGeneratingScrambles(false));
+                dispatch(resetScramblingProgressCurrent());
             });
-            let scrambleClient = new ScrambleClient(
-                this.props.updateScramblingProgressTarget,
-                this.props.updateScramblingProgressCurrentEvent
-            );
-            fetchZip(
-                scrambleClient,
-                this.props.wcif,
-                this.props.mbld,
-                this.props.password,
-                this.props.translations
-            )
-                .then((blob) => {
-                    this.props.updateFileZipBlob(blob);
-                })
-                .catch((err) => {
-                    this.interceptor.current.updateMessage(err);
-                })
-                .finally(() => {
-                    this.props.updateGeneratingScrambles(false);
-                    this.props.resetScramblingProgressCurrent();
-                });
-            this.props.updateGeneratingScrambles(true);
-        };
+        dispatch(updateGeneratingScrambles(true));
+    };
 
-        downloadZip = () => {
-            // We use the unofficialZip to stamp .zip in order to prevent delegates / organizers mistakes.
-            // If TNoodle version is not official (as per VersionInfo) or if we generate scrambles using
-            // a competition from staging, add a [Unofficial]
+    const downloadZip = () => {
+        // We use the unofficialZip to stamp .zip in order to prevent delegates / organizers mistakes.
+        // If TNoodle version is not official (as per VersionInfo) or if we generate scrambles using
+        // a competition from staging, add a [Unofficial]
 
-            let isUnofficialZip =
-                !this.props.officialZip ||
-                (this.props.competitionId != null && isUsingStaging());
+        let isUnofficialZip =
+            !officialZip || (competitionId != null && isUsingStaging());
 
-            let fileName =
-                (isUnofficialZip ? "[UNOFFICIAL] " : "") +
-                this.state.competitionNameFileZip +
-                ".zip";
+        let fileName =
+            (isUnofficialZip ? "[UNOFFICIAL] " : "") +
+            competitionNameFileZip +
+            ".zip";
 
-            const link = document.createElement("a");
-            link.href = URL.createObjectURL(this.props.fileZipBlob);
-            link.download = fileName;
-            link.target = "_blank";
-            link.setAttribute("type", "hidden");
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(fileZipBlob);
+        link.download = fileName;
+        link.target = "_blank";
+        link.setAttribute("type", "hidden");
 
-            // This is needed for firefox
-            document.body.appendChild(link);
+        // This is needed for firefox
+        document.body.appendChild(link);
 
-            link.click();
-            link.remove();
-        };
+        link.click();
+        link.remove();
+    };
 
-        scrambleButton = () => {
-            if (this.props.generatingScrambles) {
-                return (
-                    <button
-                        className="btn btn-primary button-transparent form-control"
-                        title="Wait until the process is done"
-                        disabled
-                    >
-                        Generating Scrambles
-                    </button>
-                );
-            }
-            if (this.props.fileZipBlob != null) {
-                return (
-                    <button
-                        type="submit"
-                        className="btn btn-success form-control"
-                    >
-                        Download Scrambles
-                    </button>
-                );
-            }
-
-            // At least 1 events must have at least 1 round.
-            let disableScrambleButton = !this.props.wcif.events
-                .map((event) => event.rounds.length > 0)
-                .reduce((flag1, flag2) => flag1 || flag2, false);
-
-            // In case the user did not select any events, we make the button a little more transparent than disabled
-            let btnClass =
-                "btn btn-primary form-control" +
-                (disableScrambleButton ? " button-transparent" : "");
+    const scrambleButton = () => {
+        if (generatingScrambles) {
             return (
                 <button
-                    type="submit"
-                    className={btnClass}
-                    disabled={disableScrambleButton}
-                    title={disableScrambleButton ? "No events selected." : ""}
+                    className="btn btn-primary button-transparent form-control"
+                    title="Wait until the process is done"
+                    disabled
                 >
-                    Generate Scrambles
+                    Generating Scrambles
                 </button>
             );
-        };
-
-        render() {
+        }
+        if (!!fileZipBlob) {
             return (
-                <form onSubmit={this.onSubmit}>
-                    <div className="sticky-top bg-light">
-                        <Interceptor ref={this.interceptor} />
-                        <VersionInfo />
-                        <div className="container-fluid pt-2">
-                            <div className="row">
-                                <EntryInterface />
-                                <div className="col-sm-4 form-group">
-                                    <label>&nbsp;</label>
-                                    {this.scrambleButton()}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <EventPickerTable />
-                </form>
+                <button type="submit" className="btn btn-success form-control">
+                    Download Scrambles
+                </button>
             );
         }
-    }
-);
+
+        // At least 1 events must have at least 1 round.
+        let disableScrambleButton = !wcif.events
+            .map((event) => event.rounds.length > 0)
+            .reduce((flag1, flag2) => flag1 || flag2, false);
+
+        // In case the user did not select any events, we make the button a little more transparent than disabled
+        let btnClass =
+            "btn btn-primary form-control" +
+            (disableScrambleButton ? " button-transparent" : "");
+        return (
+            <button
+                type="submit"
+                className={btnClass}
+                disabled={disableScrambleButton}
+                title={disableScrambleButton ? "No events selected." : ""}
+            >
+                Generate Scrambles
+            </button>
+        );
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="sticky-top bg-light">
+                <Interceptor ref={interceptorRef} />
+                <VersionInfo />
+                <div className="container-fluid pt-2">
+                    <div className="row">
+                        <EntryInterface />
+                        <div className="col-sm-4 form-group">
+                            <label>&nbsp;</label>
+                            {scrambleButton()}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <EventPickerTable />
+        </form>
+    );
+};
 
 export default Main;
