80dba781d18eb1c3141b27a935e05e04e29535c4
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-04 02:48:33 -0300

    Migrate tnoodle api to ts

--- tnoodle-ui/src/main/api/tnoodle.api.ts
+++ tnoodle-ui/src/main/api/tnoodle.api.ts
@@ -1,4 +1,7 @@
 import Axios from "axios";
+import Translation from "../model/Translation";
+import Wcif from "../model/Wcif";
+import { ScrambleClient } from "./tnoodle.socket";
 
 let backendUrl = new URL("http://localhost:2014");
 export const tNoodleBackend = backendUrl.toString().replace(/\/$/g, "");
@@ -11,7 +14,7 @@ let bestMbldAttemptEndpoint = "/frontend/mbld/best";
 let wcaEventsEndpoint = "/frontend/data/events";
 let formatsEndpoint = "/frontend/data/formats";
 
-const convertToBlob = async (result) => {
+const convertToBlob = async (result: any) => {
     let { contentType, payload } = result;
     let res = await fetch(`data:${contentType};base64,${payload}`);
 
@@ -22,8 +25,8 @@ const convertToBlob = async (result) => {
  * Builds the object expected for FMC translations
  * @param {array} translations e.g. ["de", "da", "pt-BR"]
  */
-const fmcTranslationsHelper = (translations) => {
-    if (translations == null) {
+const fmcTranslationsHelper = (translations?: Translation[]) => {
+    if (!translations) {
         return null;
     }
     return {
@@ -38,10 +41,10 @@ class TnoodleApi {
 
     fetchFormats = () => Axios.get(tNoodleBackend + formatsEndpoint);
 
-    fetchSuggestedFmcTranslations = (wcif) =>
+    fetchSuggestedFmcTranslations = (wcif: Wcif) =>
         Axios.post(tNoodleBackend + suggestedFmcTranslationsEndpoint, wcif);
 
-    fetchBestMbldAttempt = (wcif) =>
+    fetchBestMbldAttempt = (wcif: Wcif) =>
         Axios.post(tNoodleBackend + bestMbldAttemptEndpoint, wcif);
 
     fetchRunningVersion = () => Axios.get(tNoodleBackend + versionEndpoint);
@@ -49,16 +52,19 @@ class TnoodleApi {
     fetchAvailableFmcTranslations = () =>
         Axios.get(tNoodleBackend + fmcTranslationsEndpoint);
 
-    fetchZip = (scrambleClient, wcif, mbld, password, translations) => {
+    fetchZip = (
+        scrambleClient: ScrambleClient,
+        wcif: Wcif,
+        mbld: string,
+        password: string,
+        translations?: Translation[]
+    ) => {
         let payload = {
             wcif,
             multiCubes: { requestedScrambles: mbld },
             fmcLanguages: fmcTranslationsHelper(translations),
+            password,
         };
-
-        if (!!password) {
-            payload.zipPassword = password;
-        }
 
         return scrambleClient
             .loadScrambles(zipEndpoint, payload, wcif.id)
