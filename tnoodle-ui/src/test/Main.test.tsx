952eb51ff171cd7bb191c298ec40ea847d9289f1
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-08 00:23:04 -0300

    Fix main test

--- tnoodle-ui/src/test/Main.test.tsx
+++ tnoodle-ui/src/test/Main.test.tsx
@@ -3,35 +3,34 @@ import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
+import tnoodleApi from "../main/api/tnoodle.api";
+import wcaApi from "../main/api/wca.api";
 import Main from "../main/components/Main";
 import {
-    addSuggestedFmcTranslations,
-    updateTranslations,
-} from "../main/redux/ActionCreators";
-import store from "../main/redux/Store";
+    setSuggestedFmcTranslations,
+    setTranslations,
+} from "../main/redux/slice/FmcSlice";
 import {
     events,
     formats,
     languages,
     version,
 } from "./mock/tnoodle.api.test.mock";
+import { axiosResponse, getNewStore } from "./mock/util.test.mock";
 import { scrambleProgram } from "./mock/wca.api.test.mock";
 
-const tnoodleApi = require("../main/api/tnoodle.api");
-const wcaApi = require("../main/api/wca.api");
-
-let container = null;
+let container = document.createElement("div");
 beforeEach(() => {
     // setup a DOM element as a render target
     container = document.createElement("div");
     document.body.appendChild(container);
 
     jest.spyOn(tnoodleApi, "fetchRunningVersion").mockImplementation(() =>
-        Promise.resolve(version)
+        Promise.resolve({ data: version, ...axiosResponse })
     );
 
     jest.spyOn(wcaApi, "fetchVersionInfo").mockImplementation(() =>
-        Promise.resolve(scrambleProgram)
+        Promise.resolve({ data: scrambleProgram, ...axiosResponse })
     );
 });
 
@@ -39,44 +38,53 @@ afterEach(() => {
     // cleanup on exiting
     unmountComponentAtNode(container);
     container.remove();
-    container = null;
+    container = document.createElement("div");
 
-    tnoodleApi.fetchRunningVersion.mockRestore();
-    wcaApi.fetchVersionInfo.mockRestore();
+    jest.spyOn(tnoodleApi, "fetchRunningVersion").mockRestore();
+    jest.spyOn(wcaApi, "fetchVersionInfo").mockRestore();
 });
 
 it("There should be only 1 button of type submit, check FMC changes", async () => {
+    const store = getNewStore();
+
     // Turn on mocking behavior
     jest.spyOn(tnoodleApi, "fetchWcaEvents").mockImplementation(() =>
-        Promise.resolve(events)
+        Promise.resolve({
+            data: events,
+            ...axiosResponse,
+        })
     );
 
     jest.spyOn(tnoodleApi, "fetchFormats").mockImplementation(() =>
-        Promise.resolve(formats)
+        Promise.resolve({ data: formats, ...axiosResponse })
     );
 
     jest.spyOn(
         tnoodleApi,
         "fetchAvailableFmcTranslations"
-    ).mockImplementation(() => Promise.resolve(languages));
+    ).mockImplementation(() =>
+        Promise.resolve({ data: languages, ...axiosResponse })
+    );
 
     // We add suggested FMC so the button Select Suggested appears as well
-    const translations = Object.keys(languages).map((translationId) => ({
-        id: translationId,
-        display: languages[translationId],
+    const translations = Object.entries(languages).map(([id, name]) => ({
+        id,
+        name,
         status: true,
     }));
     const suggestedFmcTranslations = ["de", "en", "pt-BR"];
 
-    store.dispatch(updateTranslations(translations));
-    store.dispatch(addSuggestedFmcTranslations(suggestedFmcTranslations));
+    store.dispatch(setTranslations(translations));
+    store.dispatch(setSuggestedFmcTranslations(suggestedFmcTranslations));
 
     // Render component
     await act(async () => {
         render(
-            <Provider store={store}>
-                <Main />
-            </Provider>,
+            <React.StrictMode>
+                <Provider store={store}>
+                    <Main />
+                </Provider>
+            </React.StrictMode>,
             container
         );
     });
@@ -87,25 +95,32 @@ it("There should be only 1 button of type submit, check FMC changes", async () =
 
     // Change all rounds from 0 to 1 (side effect, change 3x3x3 from Ao5 to another value)
     // the objective here is to open all events
-    selects.forEach((select) => {
-        fireEvent.change(select, {
-            target: { value: "1" },
+    for (let i = 0; i < selects.length; i++) {
+        let select = selects[i];
+        await act(async () => {
+            fireEvent.change(select, {
+                target: { value: "1" },
+            });
         });
-    });
+    }
 
     // Take the form and all the buttons inside of it
-    const form = container.querySelector("form");
+    const form = container.querySelector("form")!;
     const buttons = Array.from(form.querySelectorAll("button"));
 
     // Click almost all buttons. The point here is to open translations,
     // but it won't hurt click other buttons as well.
     // By avoiding Generate Scrambles button, we avoid triggering zip generation,
     // therefore button text, which is used later.
-    buttons
-        .filter((button) => button.innerHTML !== "Generate Scrambles")
-        .forEach((button) => {
+    let almostAllButtons = buttons.filter(
+        (button) => button.innerHTML !== "Generate Scrambles"
+    );
+    for (let i = 0; i < almostAllButtons.length; i++) {
+        let button = almostAllButtons[i];
+        await act(async () => {
             fireEvent.click(button);
         });
+    }
 
     const completeButtons = Array.from(form.querySelectorAll("button"));
     const buttonsTypeSubmit = completeButtons.filter(
@@ -119,47 +134,59 @@ it("There should be only 1 button of type submit, check FMC changes", async () =
     expect(buttonsTypeSubmit[0].innerHTML).toBe("Generate Scrambles");
 
     // At first, all translations should be selected
-    store.getState().translations.forEach((translation) => {
+    store.getState().fmcSlice.translations!.forEach((translation) => {
         expect(translation.status).toEqual(true);
     });
 
     // Select suggested
-    fireEvent.click(completeButtons[completeButtons.length - 1]);
-    store.getState().translations.forEach((translation) => {
+    await act(async () => {
+        fireEvent.click(completeButtons[completeButtons.length - 1]);
+    });
+
+    store.getState().fmcSlice.translations!.forEach((translation) => {
         expect(translation.status).toEqual(
             suggestedFmcTranslations.indexOf(translation.id) >= 0
         );
     });
 
     // Select None
-    fireEvent.click(completeButtons[completeButtons.length - 2]);
-    store.getState().translations.forEach((translation) => {
+    await act(async () => {
+        fireEvent.click(completeButtons[completeButtons.length - 2]);
+    });
+    store.getState().fmcSlice.translations!.forEach((translation) => {
         expect(translation.status).toEqual(false);
     });
 
     // Select All
-    fireEvent.click(completeButtons[completeButtons.length - 3]);
-    store.getState().translations.forEach((translation) => {
+    await act(async () => {
+        fireEvent.click(completeButtons[completeButtons.length - 3]);
+    });
+    store.getState().fmcSlice.translations!.forEach((translation) => {
         expect(translation.status).toEqual(true);
     });
 
     // Here, we test just a single random language toggle
     let index = Math.floor(Math.random() * Object.keys(languages).length);
-    const checkbox = container.querySelectorAll("input[type=checkbox]")[index];
-    expect(checkbox.id).toBe("fmc-" + store.getState().translations[index].id);
+    const checkbox = Array.from(
+        container.querySelectorAll("input[type=checkbox]")
+    )[index] as HTMLInputElement;
+    expect(checkbox.id).toBe(
+        "fmc-" + store.getState().fmcSlice.translations![index].id
+    );
 
     // Check toggle behavior and its value in the store
     expect(checkbox.checked).toBe(true);
-    expect(store.getState().translations[index].status).toBe(true);
+    expect(store.getState().fmcSlice.translations![index].status).toBe(true);
     fireEvent.click(checkbox);
     expect(checkbox.checked).toBe(false);
-    expect(store.getState().translations[index].status).toBe(false);
+    expect(store.getState().fmcSlice.translations![index].status).toBe(false);
     fireEvent.click(checkbox);
     expect(checkbox.checked).toBe(true);
-    expect(store.getState().translations[index].status).toBe(true);
+    expect(store.getState().fmcSlice.translations![index].status).toBe(true);
 
     // Clear mock fetchWcaEvents
-    tnoodleApi.fetchWcaEvents.mockRestore();
-    tnoodleApi.fetchFormats.mockRestore();
-    tnoodleApi.fetchAvailableFmcTranslations.mockRestore();
+    // Clear mock
+    jest.spyOn(tnoodleApi, "fetchWcaEvents").mockRestore();
+    jest.spyOn(tnoodleApi, "fetchFormats").mockRestore();
+    jest.spyOn(tnoodleApi, "fetchAvailableFmcTranslations").mockRestore();
 });
