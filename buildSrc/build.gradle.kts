95cd0417e629c867f615d1cebe261e6a6cb7d5a4
Gregor Billing <suushiemaniac@gmail.com>
2021-06-27 23:31:14 +0200

    Bump dependency versions including Kotlin 1.5.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.bouncycastle:bcprov-jdk15on:1.68")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.69")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.11.0.202103091610-r")
 }
