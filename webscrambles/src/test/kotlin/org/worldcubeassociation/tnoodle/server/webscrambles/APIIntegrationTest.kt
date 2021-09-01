e2bc37442bc5596c4e2176c1d6adc6d1fbc661b4
Gregor Billing <gbilling@worldcubeassociation.org>
2021-09-01 15:26:22 +0200

    Parallelize testing routines for all upcoming comps

--- webscrambles/src/test/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/APIIntegrationTest.kt
+++ webscrambles/src/test/kotlin/org/worldcubeassociation/tnoodle/server/webscrambles/APIIntegrationTest.kt
@@ -1,11 +1,12 @@
 package org.worldcubeassociation.tnoodle.server.webscrambles
 
+import kotlinx.coroutines.*
 import kotlinx.serialization.builtins.ListSerializer
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.ThrowingSupplier
-import org.worldcubeassociation.tnoodle.server.serial.JsonConfig
 import org.worldcubeassociation.tnoodle.server.model.EventData
+import org.worldcubeassociation.tnoodle.server.serial.JsonConfig
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.UpcomingCompetition
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFDataBuilder
 import org.worldcubeassociation.tnoodle.server.webscrambles.wcif.WCIFScrambleMatcher
@@ -16,46 +17,44 @@ import java.time.LocalDateTime
 import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
-object APIIntegrationTest {
+class APIIntegrationTest {
     @Test
-    fun `test that every upcoming competition produces some output without error`() {
-        val competitionsLimit = 10
-        val upcomingRaw = URL("https://www.worldcubeassociation.org/api/v0/competitions/").readText()
-        val upcomingComps = JsonConfig.SERIALIZER.decodeFromString(ListSerializer(UpcomingCompetition.serializer()), upcomingRaw).shuffled().take(competitionsLimit)
+    fun `test that every upcoming competition produces some output without error`() = runBlocking {
+        val upcomingRaw = withContext(Dispatchers.IO) { URL("https://www.worldcubeassociation.org/api/v0/competitions/").readText() }
+        val upcomingComps = JsonConfig.SERIALIZER.decodeFromString(ListSerializer(UpcomingCompetition.serializer()), upcomingRaw).shuffled()
 
         val generationDate = LocalDateTime.now()
-        val testCompCount = upcomingComps.size
 
-        for ((i, upcomingComp) in upcomingComps.withIndex()) {
-            val id = upcomingComp.id
-            println("(${i+1}/$testCompCount) Testing ZIP builder for '${upcomingComp.name}' (id $id)")
+        val jobs = upcomingComps.map { it.id }.map {
+            launch(Dispatchers.Default) {
+                val url = "https://www.worldcubeassociation.org/api/v0/competitions/$it/wcif/public"
 
-            val url = "https://www.worldcubeassociation.org/api/v0/competitions/$id/wcif/public"
+                val wcifRaw = withContext(Dispatchers.IO) { URL(url).readText() }
+                val multiCubes = MultiScrambleCountExtension(Random.nextInt(10, 20))
 
-            val wcifRaw = URL(url).readText()
-            val multiCubes = MultiScrambleCountExtension(Random.nextInt(10, 20))
+                val websiteWcif = JsonConfig.SERIALIZER.decodeFromString(Competition.serializer(), wcifRaw)
+                val blankWcif = WCIFScrambleMatcher.installExtensionForEvents(websiteWcif, multiCubes, EventData.THREE_MULTI_BLD)
 
-            val websiteWcif = JsonConfig.SERIALIZER.decodeFromString(Competition.serializer(), wcifRaw)
-            val blankWcif = WCIFScrambleMatcher.installExtensionForEvents(websiteWcif, multiCubes, EventData.THREE_MULTI_BLD)
+                println("[$it] Fetched WCIF. On to computing scrambles…")
+                val scrambledWcif = WCIFScrambleMatcher.fillScrambleSets(blankWcif)
 
-            println("Fetched WCIF. On to computing scrambles…")
-            val scrambledWcif = WCIFScrambleMatcher.fillScrambleSets(blankWcif)
+                println("[$it] Scrambles generated successfully. On to rendering the PDF…")
+                val completePdf = WCIFDataBuilder.wcifToCompletePdf(scrambledWcif, generationDate.toLocalDate(), "JUnit-Test", Translate.DEFAULT_LOCALE)
+                val renderedPdf = Assertions.assertDoesNotThrow(ThrowingSupplier { completePdf.render() })
+                Assertions.assertTrue(renderedPdf.isNotEmpty())
 
-            println("Scrambles generated successfully. On to rendering the PDF…")
-            val completePdf = WCIFDataBuilder.wcifToCompletePdf(scrambledWcif, generationDate.toLocalDate(), "JUnit-Test", Translate.DEFAULT_LOCALE)
-            val renderedPdf = Assertions.assertDoesNotThrow(ThrowingSupplier { completePdf.render() })
-            Assertions.assertTrue(renderedPdf.isNotEmpty())
+                val computationTime = measureTimeMillis {
+                    println("[$it] Single PDF rendered successfully. On to compiling the ZIP…")
+                    val completeZip = WCIFDataBuilder.wcifToZip(scrambledWcif, null, generationDate, "JUnit-Test", "https://test.local")
+                    Assertions.assertTrue(completeZip.allFiles.isNotEmpty())
+                    val compiledZip = Assertions.assertDoesNotThrow(ThrowingSupplier { completeZip.compress() })
+                    Assertions.assertTrue(compiledZip.isNotEmpty())
+                }
 
-            val computationTime = measureTimeMillis {
-                println("Single PDF rendered successfully. On to compiling the ZIP…")
-                val completeZip = WCIFDataBuilder.wcifToZip(scrambledWcif, null, generationDate, "JUnit-Test", "https://test.local")
-                Assertions.assertTrue(completeZip.allFiles.isNotEmpty())
-                val compiledZip = Assertions.assertDoesNotThrow(ThrowingSupplier { completeZip.compress() })
-                Assertions.assertTrue(compiledZip.isNotEmpty())
+                println("[$it] ZIP compilation was successful! Took $computationTime ms")
             }
-
-            println("ZIP compilation was successful! Took $computationTime ms")
-            println()
         }
+
+        jobs.joinAll()
     }
 }
