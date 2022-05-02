0dfe740ea169ab2ddb159635d8b65def554a7237
Gregor Billing <gbilling@worldcubeassociation.org>
2022-05-02 14:35:39 +0900

    Gradle dependency updates

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -3,14 +3,14 @@ package dependencies
 object Versions {
     val JUNIT_JUPITER = "5.8.2"
     val BATIK = "1.14"
-    val KOTLIN = "1.6.10"
+    val KOTLIN = "1.6.21"
     val KTOR = "1.6.1"
-    val PROGUARD = "7.2.0"
+    val PROGUARD = "7.2.1"
     val KOTLESS = "0.1.6"
 
     val MARKDOWNJ_CORE = "0.4"
-    val ZIP4J = "2.9.1"
-    val ITEXTPDF = "5.5.13.2"
+    val ZIP4J = "2.10.0"
+    val ITEXTPDF = "5.5.13.3"
     val BATIK_TRANSCODER = BATIK
     val BATIK_CODEC = BATIK
     val SNAKEYAML = "1.30"
@@ -19,7 +19,7 @@ object Versions {
     val JUNIT_JUPITER_API = JUNIT_JUPITER
     val JUNIT_JUPITER_ENGINE = JUNIT_JUPITER
     val KOTLIN_SERIALIZATION_JSON = "1.3.2"
-    val KOTLIN_COROUTINES_CORE = "1.6.0"
+    val KOTLIN_COROUTINES_CORE = "1.6.1"
     val KTOR_SERVER_NETTY = KTOR
     val KTOR_SERVER_SERVLET = KTOR
     val KTOR_SERIALIZATION = KTOR
@@ -30,16 +30,16 @@ object Versions {
     val PROGUARD_GRADLE = PROGUARD
     val WCA_I18N = "0.4.3"
     val GOOGLE_APPENGINE_GRADLE = "2.4.2"
-    val GOOGLE_CLOUD_STORAGE = "2.4.1"
+    val GOOGLE_CLOUD_STORAGE = "2.6.1"
     val TNOODLE_SCRAMBLES = "0.18.1"
     val APACHE_COMMONS_LANG3 = "3.12.0"
     val KOTLESS_KTOR = KOTLESS
-    val TESTING_MOCKK = "1.12.2"
-    val KOTLINX_ATOMICFU_GRADLE = "0.17.1"
+    val TESTING_MOCKK = "1.12.3"
+    val KOTLINX_ATOMICFU_GRADLE = "0.17.2"
 
     object Plugins {
         val SHADOW = "7.1.2"
-        val NODEJS = "3.2.0"
+        val NODEJS = "3.2.1"
         val DEPENDENCY_VERSIONS = "0.42.0"
 
         val KOTLIN = Versions.KOTLIN
