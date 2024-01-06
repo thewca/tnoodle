37602a4118d5a680f4e2d10b5d252e180d781e64
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2024-01-05 23:29:05 -0300

    Add ECR and ECS service

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -1,6 +1,28 @@
-resource "aws_ecs_cluster" "keycloak_cluster" {
+resource "aws_ecs_cluster" "tnoodle_cluster" {
   name = "tnoodle-cluster"
 
+  tags = {
+    (var.type) = var.type_ecs
+  }
+}
+
+resource "aws_ecs_task_definition" "tnoodle_task_definition" {
+  family                   = "tnoodle-task-definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = templatefile("./templates/container-definitions/tnoodle.json", {
+    app_image      = aws_ecr_repository.tnoodle.repository_url
+    aws_region     = var.aws_region
+    app_port       = var.tnoodle_port
+    container_name = var.tnoodle_name
+    fargate_cpu    = var.fargate_cpu
+    fargate_memory = var.fargate_memory
+  })
   tags = {
     (var.type) = var.type_ecs
   }
--- iac/variable.tf
+++ iac/variable.tf
@@ -5,3 +5,27 @@ variable "type" {
 variable "type_ecs" {
   default = "ECS"
 }
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "tnoodle_name" {
+  default = "tnoodle"
+}
+
+variable "type_ecr" {
+  default = "ECR"
+}
+
+variable "tnoodle_port" {
+  default = "2014"
+}
+
+variable "fargate_cpu" {
+  default = "1024" # 1 vCPU
+}
+
+variable "fargate_memory" {
+  default = "2048" # 2 GB
+}
